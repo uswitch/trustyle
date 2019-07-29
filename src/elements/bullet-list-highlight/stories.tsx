58714c78bde6d32d41cbf587f1a9b7e58f5ce910
Daniel Whyte <danwhy@gmail.com>
2019-07-29 14:35:40 +0100

    bump

--- src/elements/bullet-list-highlight/stories.tsx
+++ src/elements/bullet-list-highlight/stories.tsx
@@ -7,7 +7,7 @@ storiesOf('Elements|Bullet List Highlight', module).add('Example', () => (
   <BulletListHighlight
     highlights={[
       <span key="example">
-        You will pay £82.15 a month by Direct Debit to British Gas.
+        You will pay £83.15 a month by Direct Debit to British Gas.
       </span>,
       <span key="example2">
         Your first payment will be taken on or shortly after your supply start
