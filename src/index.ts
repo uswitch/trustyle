4722149f012e13163792cf7e7449af97fe944741
Daniel Whyte <danwhy@gmail.com>
2019-07-29 14:29:07 +0100

    makes packages public

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
-import { Button } from './elements/button'
 import { Icon } from './elements/icon'
 import * as typography from './styles/typography'
 
 export {
   // Components
-  Button,
   Icon,
   // Styles
   typography
