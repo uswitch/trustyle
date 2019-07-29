d4f24c16a9492b52a171c4941b20e457eed4da56
Michael Raymond <raymond.michael@gmail.com>
2019-07-29 17:14:05 +0100

    bah

---
+++ src/styles/index.ts
@@ -0,0 +1,11 @@
+import * as colors from "./colors"
+import * as global from "./global"
+import * as helpers from "./helpers"
+import * as typography from "./typography"
+
+export {
+  colors,
+  global,
+  helpers,
+  typography
+}
