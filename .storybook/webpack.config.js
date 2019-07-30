30dc5fae7e4f0ea5543e9808411d62d14283a1cf
Michael Raymond <raymond.michael@gmail.com>
2019-07-30 15:53:53 +0100

    Export lib as flat structure

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,7 +2,10 @@ module.exports = ({ config }) => {
   config.module.rules.push({
     test: /\.tsx?$/,
     exclude: /node_modules/,
-    loader: 'babel-loader'
+    loader: 'babel-loader',
+    options: {
+      presets: ["@babel/typescript", "@babel/preset-react"]
+    }
   })
   config.resolve.extensions.push('.ts', '.tsx')
   return config
