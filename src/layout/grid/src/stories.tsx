f4cce6c2272ec4326f81c28d0a8787035daea273
Rik Jurriaans <rik@jurriaans.org>
2019-11-27 17:25:13 +0000

    WIP grid system

---
+++ src/layout/grid/src/stories.tsx
@@ -0,0 +1,10 @@
+/** @jsx jsx */
+
+import * as React from 'react'
+import { storiesOf } from '@storybook/react'
+
+storiesOf('Layout|Grid', module).add('Types', () => (
+  <div>
+    Test
+  </div>
+))
