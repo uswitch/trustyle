0e8f367bde10269d1641ae10d80a8835949d6345
Rik Jurriaans <rik@jurriaans.org>
2019-12-04 16:27:13 +0000

    Create typography styled components instead of classes for cleanness

--- src/styles/src/stories.tsx
+++ src/styles/src/stories.tsx
@@ -5,7 +5,14 @@ import { css, jsx } from '@emotion/core'
 import { storiesOf } from '@storybook/react'
 
 import * as colors from './colors'
-import * as types from './typography'
+import {
+  H2,
+  H3,
+  P,
+  Label,
+  Small,
+  A
+} from './typography'
 
 const swatch = css({
   padding: '10px',
@@ -28,10 +35,15 @@ storiesOf('Styles|Colours', module).add('Swatches', () =>
   ))
 )
 
-storiesOf('Styles|Typography', module).add('Types', () =>
-  Object.entries(types).map(([key, value]) => (
-    <p key={key} css={value}>
-      {key}
-    </p>
-  ))
-)
+storiesOf('Styles|Typography', module).add('Types', () => (
+  <div>
+    <H2>This is a h2</H2>
+    <H3>This is a h3</H3>
+    <P>This is a p tag</P>
+    <Label>Label</Label>
+    <div>
+      <A href='#'>Just a link</A>
+    </div>
+    <Small>This is a small</Small>
+  </div>
+))
--- src/styles/src/typography.ts
+++ src/styles/src/typography.ts
@@ -1,21 +1,23 @@
 import { css } from '@emotion/core'
+import styled from '@emotion/styled'
 
+import { mq } from './media-queries'
 import { battleshipGrey, black, cerulean, white } from './colors'
 import { pxToRem } from './utils'
 
 export const defaultFontFamily =
   '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", Arial, sans-serif'
 
-export const heading2 = css({
+export const H2 = styled.h2(mq({
   fontFamily: defaultFontFamily,
   fontSize: pxToRem(24),
   fontWeight: 600,
   lineHeight: '1.23',
   letterSpacing: 'normal',
   color: black
-})
+}))
 
-export const heading3 = css({
+export const H3 = styled.h3({
   fontFamily: defaultFontFamily,
   fontSize: pxToRem(18),
   fontWeight: 600,
@@ -24,39 +26,7 @@ export const heading3 = css({
   color: black
 })
 
-export const label = css({
-  fontFamily: defaultFontFamily,
-  fontSize: pxToRem(17),
-  fontWeight: 600,
-  lineHeight: '1.33',
-  letterSpacing: 'normal',
-  color: black
-})
-
-export const subheading = css({
-  fontFamily: defaultFontFamily,
-  fontSize: pxToRem(17),
-  fontWeight: 500,
-  fontStyle: 'normal',
-  fontStretch: 'normal',
-  lineHeight: 1.33,
-  letterSpacing: 'normal',
-  color: battleshipGrey
-})
-
-export const inputFontSize = 17
-export const inputLineHeight = 1.33
-
-export const input = css({
-  fontFamily: defaultFontFamily,
-  fontSize: pxToRem(inputFontSize),
-  fontWeight: 'normal',
-  lineHeight: inputLineHeight,
-  letterSpacing: 'normal',
-  color: black
-})
-
-export const text = css({
+export const P = styled.p({
   fontFamily: defaultFontFamily,
   fontSize: pxToRem(16),
   fontWeight: 'normal',
@@ -65,7 +35,19 @@ export const text = css({
   color: battleshipGrey
 })
 
-export const small = css({
+export const Label = styled.p({
+  fontFamily: defaultFontFamily,
+  fontSize: pxToRem(17),
+  fontWeight: 600,
+  lineHeight: '1.33',
+  letterSpacing: 'normal',
+  color: black
+})
+
+export const inputFontSize = 17
+export const inputLineHeight = 1.33
+
+export const Small = styled.small({
   fontFamily: defaultFontFamily,
   fontSize: pxToRem(14),
   fontWeight: 'normal',
@@ -74,7 +56,7 @@ export const small = css({
   color: battleshipGrey
 })
 
-export const linkText = css({
+export const A = styled.a({
   fontFamily: defaultFontFamily,
   color: cerulean,
   fontSize: pxToRem(16),
@@ -92,4 +74,24 @@ export const alertText = css({
 
 export const noWrap = css({
   whiteSpace: 'nowrap'
+})
+
+export const input = css({
+    fontFamily: defaultFontFamily,
+    fontSize: pxToRem(inputFontSize),
+    fontWeight: 'normal',
+    lineHeight: inputLineHeight,
+    letterSpacing: 'normal',
+    color: black
+})
+
+export const subheading = css({
+    fontFamily: defaultFontFamily,
+    fontSize: pxToRem(17),
+    fontWeight: 500,
+    fontStyle: 'normal',
+    fontStretch: 'normal',
+    lineHeight: 1.33,
+    letterSpacing: 'normal',
+    color: battleshipGrey
 })
