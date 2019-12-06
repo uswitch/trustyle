c4f3bdc267c2dc12ca601cb83647e4500c775781
Phil Helm <github@phelm.co.uk>
2019-12-06 11:29:27 +0000

    add css prop to grid components

--- src/layout/grid/src/index.tsx
+++ src/layout/grid/src/index.tsx
@@ -7,49 +7,59 @@ import { breakpoints, Breakpoints } from '@uswitch/trustyle.styles'
 import * as st from './styles'
 
 interface Props {
-  children: any;
+  children: any
   outerMargin?: string[]
+  css?: any
 }
 
-export const Container: React.FC<Props> = ({ children, outerMargin, ...props }) => (
-  <div css={st.container(outerMargin)} {...props}>
-    { children }
+export const Container: React.FC<Props> = ({
+  children,
+  outerMargin,
+  css,
+  ...props
+}) => (
+  <div css={[st.container(outerMargin), css]} {...props}>
+    {children}
   </div>
 )
 
 interface ColumnProps extends Breakpoints {
-  children: any;
+  children: any
+  css?: any
 }
 
-const parseSizeFromProps = (fn : (breakpoint: [string, any]) => any) => Object.entries(breakpoints).map(fn)
+const parseSizeFromProps = (fn: (breakpoint: [string, any]) => any) =>
+  Object.entries(breakpoints).map(fn)
 
 const parseDisplay = (props: any): string[] =>
-  parseSizeFromProps(
-    ([key, _]) => props[`hide-${key}`] ? 'none' : 'block'
-  )
+  parseSizeFromProps(([key, _]) => (props[`hide-${key}`] ? 'none' : 'block'))
 
 const parseSizes = (props: any): number[] =>
-  parseSizeFromProps(
-    ([key, _]) => parseFloat(props[key]) || 1
-  )
+  parseSizeFromProps(([key, _]) => parseFloat(props[key]) || 1)
 
-export const Column: React.FC<ColumnProps> = ({ children, ...props }) => {
+export const Column: React.FC<ColumnProps> = ({ children, css, ...props }) => {
   const sizes = parseSizes(props)
   const display = parseDisplay(props)
   return (
-    <div css={st.column(sizes, display)} {...props}>
-      { children }
+    <div css={[st.column(sizes, display), css]} {...props}>
+      {children}
     </div>
   )
 }
 
 interface RowProps {
-  children: any;
-  centerX?: boolean;
+  children: any
+  centerX?: boolean
+  css?: any
 }
 
-export const Row: React.FC<RowProps> = ({ children, centerX, ...props }) => (
-  <div css={st.row(centerX)} {...props}>
-    { children }
+export const Row: React.FC<RowProps> = ({
+  children,
+  centerX,
+  css,
+  ...props
+}) => (
+  <div css={[st.row(centerX), css]} {...props}>
+    {children}
   </div>
 )
