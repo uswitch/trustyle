efbf2a949de7e3688f38b3032c8a57419222591e
Phil Helm <github@phelm.co.uk>
2019-12-09 12:00:33 +0000

    remove negative vertical margin on Row component

--- src/layout/grid/src/styles.ts
+++ src/layout/grid/src/styles.ts
@@ -6,48 +6,54 @@ const colWidths = [32, 64, 72]
 const gutterWidths = [8, 16, 24]
 
 const containerWidths = numberOfColumnss.map((n, idx) => {
-    const val = n * colWidths[idx] + n * gutterWidths[idx]
-    if (val < Object.values(breakpoints)[1]) {
-        return '100%'
-    } else {
-        return val
-    }
+  const val = n * colWidths[idx] + n * gutterWidths[idx]
+  if (val < Object.values(breakpoints)[1]) {
+    return '100%'
+  } else {
+    return val
+  }
 })
 
-const paddings = gutterWidths.map((gw) => `${gw / 2}px`)
+const paddings = gutterWidths.map(gw => `${gw / 2}px`)
 
-export const container = (outerMargin: string[] = ['0 auto']): DynamicStyle[] => mq({
-  position: 'relative',
-  margin: outerMargin,
-  maxWidth: containerWidths,
-  width: ['auto', ...containerWidths.slice(1)],
-  overflow: 'hidden',
-  paddingLeft: paddings,
-  paddingRight: paddings
-})
+export const container = (outerMargin: string[] = ['0 auto']): DynamicStyle[] =>
+  mq({
+    position: 'relative',
+    margin: outerMargin,
+    maxWidth: containerWidths,
+    width: ['auto', ...containerWidths.slice(1)],
+    overflow: 'hidden',
+    paddingLeft: paddings,
+    paddingRight: paddings
+  })
 
 const getWidthPercentageFromSizes = (sizes: number[]) =>
-  sizes.map((size) =>  `${100 * size}%`)
+  sizes.map(size => `${100 * size}%`)
 
-export const column = (sizes: number[] = [], display: string[] = [], paddingTop: boolean = false, paddingBottom: boolean = false): DynamicStyle[] => mq({
-  boxSizing: 'border-box',
-  display,
-  paddingLeft: paddings,
-  paddingRight: paddings,
-  paddingTop: paddingTop ? paddings : [],
-  paddingBottom: paddingBottom ? paddings : [],
-  flexBasis: getWidthPercentageFromSizes(sizes)
-})
+export const column = (
+  sizes: number[] = [],
+  display: string[] = [],
+  paddingTop: boolean = false,
+  paddingBottom: boolean = false
+): DynamicStyle[] =>
+  mq({
+    boxSizing: 'border-box',
+    display,
+    paddingLeft: paddings,
+    paddingRight: paddings,
+    paddingTop: paddingTop ? paddings : [],
+    paddingBottom: paddingBottom ? paddings : [],
+    flexBasis: getWidthPercentageFromSizes(sizes)
+  })
 
-export const row = (centerX: boolean = false): DynamicStyle[] => mq({
-  boxSizing: 'border-box',
-  display: 'flex',
-  overflow: 'hidden',
-  flexDirection: 'row',
-  marginLeft: paddings.map(padding => `-${padding}`),
-  marginRight: paddings.map(padding => `-${padding}`),
-  marginTop: paddings.map(padding => `-${padding}`),
-  marginBottom: paddings.map(padding => `-${padding}`),
-  justifyContent: centerX ? 'center' : 'flex-start',
-  flexWrap: 'wrap'
-})
+export const row = (centerX: boolean = false): DynamicStyle[] =>
+  mq({
+    boxSizing: 'border-box',
+    display: 'flex',
+    overflow: 'hidden',
+    flexDirection: 'row',
+    marginLeft: paddings.map(padding => `-${padding}`),
+    marginRight: paddings.map(padding => `-${padding}`),
+    justifyContent: centerX ? 'center' : 'flex-start',
+    flexWrap: 'wrap'
+  })
