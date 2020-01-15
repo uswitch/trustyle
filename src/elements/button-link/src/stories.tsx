e2d94a1140e23d8890c5dbfbcce21a19ee5c3b5f
Callum Macrae <a-callum.macrae@rvu.co.uk>
2020-01-15 11:04:00 +0000

    Fix prettier errors in button and button-link story files

--- src/elements/button-link/src/stories.tsx
+++ src/elements/button-link/src/stories.tsx
@@ -10,21 +10,20 @@ import { ButtonLink } from './'
 
 const Spacer = () => <div css={css({ minHeight: 20 })} />
 
-storiesOf('Elements|ButtonLink', module)
-  .add('all variants', () => (
-    <div css={css({ padding: number('Padding', 10) })}>
-      {theme() &&
-        Object.keys(theme().buttons).map((key, index) => (
-          <React.Fragment key={index}>
-            <ButtonLink
-              variant={key}
-              href="https://www.uswitch.com"
-              target="_blank"
-            >
-              {text(`${key} label`, `${key} link button`)}
-            </ButtonLink>
-            <Spacer />
-          </React.Fragment>
-        ))}
-    </div>
-  ))
+storiesOf('Elements|ButtonLink', module).add('all variants', () => (
+  <div css={css({ padding: number('Padding', 10) })}>
+    {theme() &&
+      Object.keys(theme().buttons).map((key, index) => (
+        <React.Fragment key={index}>
+          <ButtonLink
+            variant={key}
+            href="https://www.uswitch.com"
+            target="_blank"
+          >
+            {text(`${key} label`, `${key} link button`)}
+          </ButtonLink>
+          <Spacer />
+        </React.Fragment>
+      ))}
+  </div>
+))
--- src/elements/button/src/stories.tsx
+++ src/elements/button/src/stories.tsx
@@ -13,29 +13,28 @@ import { Button } from './'
 
 const Spacer = () => <div css={css({ minHeight: 20 })} />
 
-storiesOf('Elements|Button', module)
-  .add('primary variant', () => (
-    <div css={css({ padding: number('Padding', 10) })}>
-      {theme() &&
-        Object.keys(theme().buttons).map((key, index) => (
-          <React.Fragment key={index}>
-            <Button
-              variant={key}
-              disabled={boolean('Disabled', false)}
-              onClick={action(`${key}-click`)}
-            >
-              {text(`${key} label`, `${key} button`)}
-              {key.match('icon') && (
-                <Icon
-                  color={colors.cobaltBlue}
-                  direction="right"
-                  glyph="caret"
-                  size={20}
-                />
-              )}
-            </Button>
-            <Spacer />
-          </React.Fragment>
-        ))}
-    </div>
-  ))
+storiesOf('Elements|Button', module).add('primary variant', () => (
+  <div css={css({ padding: number('Padding', 10) })}>
+    {theme() &&
+      Object.keys(theme().buttons).map((key, index) => (
+        <React.Fragment key={index}>
+          <Button
+            variant={key}
+            disabled={boolean('Disabled', false)}
+            onClick={action(`${key}-click`)}
+          >
+            {text(`${key} label`, `${key} button`)}
+            {key.match('icon') && (
+              <Icon
+                color={colors.cobaltBlue}
+                direction="right"
+                glyph="caret"
+                size={20}
+              />
+            )}
+          </Button>
+          <Spacer />
+        </React.Fragment>
+      ))}
+  </div>
+))
