2609fa5300792f094567a0c564ff93c29332c178
Callum Macrae <a-callum.macrae@rvu.co.uk>
2020-01-15 16:41:55 +0000

    Use lerna version lifecycle hook to update package-lock.json files
    Hasn't been tested yet.
    
    Documented here: [version lifecycle hooks], but also ran on publish:
    [publish lifecycle hooks].
    
    [version lifecycle hooks]: https://github.com/lerna/lerna/tree/master/commands/version#lifecycle-scripts
    [publish lifecycle hooks]: https://github.com/lerna/lerna/tree/master/commands/publish#lifecycle-scripts

--- publish.sh
+++ publish.sh
@@ -13,8 +13,3 @@ git checkout -b $publish_branch
 git push origin $publish_branch
 
 npx lerna publish
-
-# Update package-lock.json files as well
-npx lerna exec npm install
-git commit --all --amend --no-edit
-git push -f origin $publish_branch
