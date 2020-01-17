7c1b3e8dd4f1eef1e7ce8a29a000b3f88aa77142
Brendon Boshell <brendon.boshell@uswitch.com>
2020-01-17 17:15:32 +0000

    Update package-lock.json after lerna publish (#193)
    Currently, we run npm install after lerna version, but before the packages are published. This means that the integrity and version parts of package-lock.json cannot be updated.
    
    This PR ensures that package-lock.json are updated after lerna publish. It copies over the tags to the newly-created commit.

--- publish.sh
+++ publish.sh
@@ -12,4 +12,14 @@ publish_branch=publish_$v
 git checkout -b $publish_branch
 git push origin $publish_branch
 
-npx lerna publish
+npx lerna publish --no-push
+
+# Run `npm install` to update the package-lock.json files, copying over the git tags to the
+# new commit.
+tags=$(git tag --points-at HEAD)
+message=$(git log -1 HEAD --pretty=%B)
+npx lerna exec "npm install --ignore-scripts --package-lock-only"
+git commit --all --amend -m "(With package-lock.json changes) ${message}"
+echo "$tags" | xargs -L 1 git tag -d
+echo "$tags" | xargs -L 1 git tag
+git push --follow-tags
