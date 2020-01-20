c7671700c269741b9d2f470e0007bdf1f805f42b
Callum Macrae <a-callum.macrae@rvu.co.uk>
2020-01-20 17:46:24 +0000

    Add category title component

---
+++ src/elements/category-title/src/index.tsx
@@ -0,0 +1,23 @@
+/** @jsx jsx */
+
+import * as React from 'react'
+import { IntrinsicSxElements, jsx, Styled, useThemeUI } from 'theme-ui'
+
+interface ListProps extends React.OlHTMLAttributes<HTMLUListElement> {
+  level: 1 | 2 | 3 | 4 | 5 | 6
+}
+
+const CategoryTitle: React.FC<ListProps> = ({ level = 2, children }) => {
+  const { theme }: any = useThemeUI()
+
+  const headingElement = `h${level}` as keyof IntrinsicSxElements
+  const Heading = Styled[headingElement]
+
+  return (
+    <div sx={{ paddingX: 'md', paddingY: 'sm', ...theme.categoryTitle }}>
+      <Heading sx={{ color: 'light-1' }}>{children}</Heading>
+    </div>
+  )
+}
+
+export default CategoryTitle
