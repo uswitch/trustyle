ec20e64de5559ae6cdec2b1f758e67c446f70d5a
Barney Fox <barneyfoxuk@gmail.com>
2020-01-28 12:03:07 +0000

    set default for isInitiallyOpen

--- src/elements/accordion/src/index.tsx
+++ src/elements/accordion/src/index.tsx
@@ -5,52 +5,63 @@ import { jsx, useThemeUI } from 'theme-ui'
 import { Icon } from '@uswitch/trustyle.icon'
 
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
-  title: string,
+  title: string
   isInitiallyOpen?: boolean
 }
 
 const Accordion: React.FC<Props> = ({
   title,
-  isInitiallyOpen,
+  isInitiallyOpen = false,
   children
 }) => {
-  const { theme: { accordion: accordionTheme = {}, colors = {} } }: any = useThemeUI()
-  const [isOpen, setIsOpen] = useState(!!isInitiallyOpen)
+  const {
+    theme: { accordion: accordionTheme = {}, colors = {} }
+  }: any = useThemeUI()
+  const [isOpen, setIsOpen] = useState(isInitiallyOpen)
 
-  return <div>
-    <button 
-      sx={{
-        cursor: 'pointer',
-        border: 0,
-        display: 'flex',
-        flexDirection: 'row',
-        width: '100%',
-        ...accordionTheme.button,
-        ...(isOpen ? accordionTheme.isActive.button : {})
-      }}
-      onClick={() => setIsOpen(!isOpen)}
-    >
-      <div 
-        sx={{ 
-          flex: '1', 
-          textAlign: 'left' 
-        }}>
-        {title}
+  return (
+    <div>
+      <button
+        sx={{
+          cursor: 'pointer',
+          border: 0,
+          display: 'flex',
+          flexDirection: 'row',
+          width: '100%',
+          ...accordionTheme.button,
+          ...(isOpen ? accordionTheme.isActive.button : {})
+        }}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <div
+          sx={{
+            flex: '1',
+            textAlign: 'left'
+          }}
+        >
+          {title}
+        </div>
+        <Icon
+          color={
+            isOpen
+              ? colors[accordionTheme.isActive.caret.color]
+              : colors[accordionTheme.caret.color]
+          }
+          glyph="caret"
+          direction={isOpen ? 'up' : 'down'}
+          size={16}
+        />
+      </button>
+      <div
+        sx={{
+          overflow: 'hidden',
+          height: isOpen ? 'auto' : '0'
+        }}
+      >
+        {children}
       </div>
-      <Icon 
-        color={isOpen ? colors[accordionTheme.isActive.caret.color] : colors[accordionTheme.caret.color]}
-        glyph='caret'
-        direction={isOpen ? 'up' : 'down'}
-        size={16}
-      />
-    </button>        
-    <div sx={{
-      overflow: 'hidden',
-      height: isOpen ? 'auto' : '0',
-    }}>
-      {children}
     </div>
-  </div>
+  )
 }
 
 export default Accordion
