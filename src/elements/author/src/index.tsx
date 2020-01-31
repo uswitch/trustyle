d0b35289e09433479a6d47421caec0ede2b31a94
Callum Macrae <a-callum.macrae@rvu.co.uk>
2020-01-31 14:00:47 +0000

    Change author component to match updated design

--- src/elements/author/src/index.tsx
+++ src/elements/author/src/index.tsx
@@ -1,39 +1,42 @@
 /** @jsx jsx */
 
 import * as React from 'react'
-import { jsx, Styled, useThemeUI } from 'theme-ui'
+import { jsx, Styled } from 'theme-ui'
 import dayjs from '@uswitch/trustyle.date'
 
-interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  title: string
-  author: string
+interface Props extends React.HTMLAttributes<HTMLDivElement> {
+  name: string
+  role: string
+  editorName?: string
+  editorRole?: string
   authorImage?: React.ReactElement
   date: number
 }
 
-const Author: React.FC<Props> = ({ title, author, authorImage, date }) => {
-  const { theme }: any = useThemeUI()
-
-  const getTheme = (key: string) => theme.author && theme.author[key]
-
+const Author: React.FC<Props> = ({
+  name,
+  role,
+  editorName,
+  editorRole,
+  authorImage,
+  date
+}) => {
   return (
     <div
       sx={{
-        padding: 'md',
-        borderRadius: 1,
         display: 'flex',
-        alignItems: ['normal', 'center'],
-        flexDirection: ['column', 'row'],
-        ...getTheme('main')
+        alignItems: 'center',
+        flexDirection: 'row',
+        variant: 'author.main'
       }}
     >
       {authorImage && (
         <div
           sx={{
             flexShrink: 0,
-            height: ['80px', '110px'],
-            width: ['80px', '110px'],
-            marginRight: [0, 'md']
+            height: ['48px', '56px'],
+            width: ['48px', '56px'],
+            marginRight: 'sm'
           }}
         >
           <authorImage.type
@@ -42,38 +45,34 @@ const Author: React.FC<Props> = ({ title, author, authorImage, date }) => {
               maxHeight: '100%',
               maxWidth: '100%',
               borderRadius: '50%',
-              ...getTheme('image')
+              variant: 'author.image'
             }}
           />
         </div>
       )}
 
       <div>
-        <Styled.h4
+        <Styled.h6
           sx={{
-            marginTop: ['1em', 0],
-            ...getTheme('heading')
+            marginTop: 0,
+            marginBottom: 'xxs',
+            fontWeight: 'bold',
+            variant: 'author.heading'
           }}
+          as="h3"
         >
-          {title}
-        </Styled.h4>
+          Written by {name}, {role}
+        </Styled.h6>
         <Styled.p
           sx={{
-            fontSize: 'xs',
-            ...getTheme('text')
+            fontSize: 'xxs',
+            marginY: 0,
+            variant: 'author.text'
           }}
         >
-          Updated on {dayjs(date).format('LL')}
+          {editorName && `Edited by ${editorName}, ${editorRole}, `}
+          {dayjs(date).format('LL')}
         </Styled.p>
-        <Styled.a
-          sx={{
-            color: 'dark-1',
-            fontSize: ['xs', 'base'],
-            ...getTheme('link')
-          }}
-        >
-          More from {author}
-        </Styled.a>
       </div>
     </div>
   )
--- src/elements/author/src/stories.tsx
+++ src/elements/author/src/stories.tsx
@@ -7,15 +7,19 @@ import { date, text } from '@storybook/addon-knobs'
 import Author from './'
 
 storiesOf('Elements|Author', module).add('Example', () => {
-  const titleText = text('Title text', 'Written by Salman, Posted by Salman')
-  const author = text('Author name', 'Salman')
+  const name = text('Author name', 'Salman')
+  const role = text('Author role', 'Job title')
+  const editorName = text('Editor name', 'Sarah Guershon')
+  const editorRole = text('Editor role', 'Job title')
   const image = <img src="https://placekitten.com/200/200" alt="Salman" />
   const updatedDate = date('Article date', new Date(2020, 2, 15))
 
   return (
     <Author
-      title={titleText}
-      author={author}
+      name={name}
+      role={role}
+      editorName={editorName}
+      editorRole={editorRole}
       authorImage={image}
       date={updatedDate}
     />
