0adfb3065cf89814dc3a9b74c18697b136ed7027
Sam Egger <segger87@gmail.com>
2020-02-03 15:24:52 +0000

    linting

--- src/elements/embedded-video/src/stories.tsx
+++ src/elements/embedded-video/src/stories.tsx
@@ -2,18 +2,12 @@
 import * as React from 'react'
 import { jsx } from '@emotion/core'
 import { storiesOf } from '@storybook/react'
-import { ThemeProvider } from 'theme-ui'
-
-import theme from '../../../utils/theme-selector'
 
 import EmbeddedVideo from './'
 
 const UswitchAdvert = 'https://www.youtube.com/embed/1cy0Buice_0'
 
 storiesOf('Elements|embedded-video', module)
-  .addDecorator(Story => {
-    return <ThemeProvider theme={theme()}>{Story()}</ThemeProvider>
-  })
   .add('primary variant', () => (
     <EmbeddedVideo
       source={UswitchAdvert}
