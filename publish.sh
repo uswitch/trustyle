6dd0a7fab500254e5ca956d5e9c411a995374030
Callum Macrae <a-callum.macrae@rvu.co.uk>
2020-02-04 11:09:18 +0000

    Maybe fix publish script so that it doesn't update everything every time

--- publish.sh
+++ publish.sh
@@ -1,7 +1,18 @@
 #!/bin/bash -ex
 
+# HOW TO NOT BREAK THIS SCRIPT:
+#
+# [1]: npm install is being ran in the packages twice - once before lerna
+# publish to make sure everything is up to date, and then again afterwards to
+# update the package-lock.json files, which are then committed.
+#
+# [2]: Lerna uses the git tags to work out what has changed. They must be
+# annotated tags or lerna will think every package has always been updated:
+# https://github.com/lerna/lerna/issues/1357#issuecomment-438162152
+
 git checkout master
 git pull origin --rebase
+git pull --tags
 npx lerna exec npm install
 npx lerna bootstrap
 npx lerna run build
@@ -14,13 +25,13 @@ git push origin $publish_branch
 
 npx lerna publish --no-push
 
-# Run `npm install` to update the package-lock.json files, copying over the git tags to the
-# new commit.
 tags=$(git tag --points-at HEAD)
 message=$(git log -1 HEAD --pretty=%B)
-npx lerna exec "npm install --ignore-scripts --package-lock-only"
+npx lerna exec "npm install --ignore-scripts --package-lock-only" # [1]
 git commit --all --amend -m "(With package-lock.json changes) ${message}"
-echo "$tags" | xargs -L 1 git tag -d
-echo "$tags" | xargs -L 1 git tag
+while IFS= read -r tag; do
+  git tag -d "$tag"
+  git tag "$tag" -m "$tag" # [2]
+done <<< "$tags"
 git push origin $publish_branch
 git push origin $publish_branch --tags
