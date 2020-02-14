0498b0dfe7ed3072acee7fd826fa45abebb71691
Salomao Rodrigues <salomao88@gmail.com>
2020-02-14 16:43:49 +0000

    Convert radio input to use theming

--- src/elements/radio-input/src/index.tsx
+++ src/elements/radio-input/src/index.tsx
@@ -1,9 +1,7 @@
 /** @jsx jsx */
 
 import * as React from 'react'
-import { jsx } from '@emotion/core'
-
-import * as st from './styles'
+import { jsx } from 'theme-ui'
 
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string
@@ -11,8 +9,48 @@ interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
 }
 
 export const RadioInput: React.FC<Props> = ({ label, ...inputProps }) => (
-  <label css={st.label}>
-    <input css={st.input} type="radio" {...inputProps} />
-    <span css={st.span}>{label}</span>
+  <label sx={{ display: 'block' }}>
+    <input
+      sx={{
+        appearance: 'none',
+        position: 'absolute',
+        '&:checked + span': {
+          '&::before': {
+            transition: 'background-color 200ms, border-color 200ms'
+          }
+        },
+        variant: 'input.radio.base'
+      }}
+      type="radio"
+      {...inputProps}
+    />
+    <span
+      sx={{
+        alignItems: 'center',
+        boxSizing: 'border-box',
+        cursor: 'pointer',
+        display: 'flex',
+        minHeight: '50px',
+        padding: 'base',
+        paddingLeft: 0,
+        width: '100%',
+        '&::before': {
+          borderRadius: '50%',
+          borderStyle: 'solid',
+          borderWidth: '2px',
+          content: '" "',
+          display: 'block',
+          height: '17px',
+          marginLeft: 'base',
+          marginRight: 'base',
+          width: '17px',
+          flexGrow: 0,
+          flexShrink: 0
+        },
+        variant: 'input.radio.label'
+      }}
+    >
+      {label}
+    </span>
   </label>
 )
