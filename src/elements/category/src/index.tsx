35938f013a1fe5b9337b9fb198cf355103643a4f
Callum Macrae <a-callum.macrae@rvu.co.uk>
2020-02-24 16:48:49 +0000

    Added container to category component

--- src/elements/category/src/index.tsx
+++ src/elements/category/src/index.tsx
@@ -6,9 +6,15 @@ import { jsx, Styled } from 'theme-ui'
 interface ListProps extends React.HTMLAttributes<HTMLDivElement> {
   title: string
   text?: string
+  container?: React.FC
 }
 
-const Category: React.FC<ListProps> = ({ title, text }) => {
+const Category: React.FC<ListProps> = ({ title, text, container }) => {
+  const DefaultContainer: React.FC = ({ children }) => (
+    <div sx={{ maxWidth: 1024, margin: '0 auto' }}>{children}</div>
+  )
+  const Container: React.FC = container || DefaultContainer
+
   return (
     <div
       sx={{
@@ -17,21 +23,23 @@ const Category: React.FC<ListProps> = ({ title, text }) => {
         variant: 'categoryTitle.main'
       }}
     >
-      <Styled.h1
-        as={text ? 'h1' : 'p'}
-        sx={{
-          padding: 0,
-          margin: 0,
-          variant: 'categoryTitle.heading'
-        }}
-      >
-        {title}
-      </Styled.h1>
-      {text && (
-        <Styled.p sx={{ marginBottom: 0, variant: 'categoryTitle.text' }}>
-          {text}
-        </Styled.p>
-      )}
+      <Container>
+        <Styled.h1
+          as={text ? 'h1' : 'p'}
+          sx={{
+            padding: 0,
+            margin: 0,
+            variant: 'categoryTitle.heading'
+          }}
+        >
+          {title}
+        </Styled.h1>
+        {text && (
+          <Styled.p sx={{ marginBottom: 0, variant: 'categoryTitle.text' }}>
+            {text}
+          </Styled.p>
+        )}
+      </Container>
     </div>
   )
 }
