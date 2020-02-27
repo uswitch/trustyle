2cbdf4f31b67f7193a7bc097c792bb5e93c72500
Joshua Croad <joshcroad@gmail.com>
2020-02-27 12:09:38 +0000

    Add sr-only variant to theme

--- src/elements/toggle-switch/src/index.tsx
+++ src/elements/toggle-switch/src/index.tsx
@@ -24,14 +24,7 @@ const ToggleSwitch: React.FC<Props> = ({
       onChange={onChange}
       {...props}
       sx={{
-        position: 'absolute',
-        width: 1,
-        height: 1,
-        padding: 0,
-        margin: -1,
-        overflow: 'hidden',
-        clip: 'rect(0, 0, 0, 0)',
-        border: 0
+        variant: 'sr-only'
       }}
       type="checkbox"
     />
