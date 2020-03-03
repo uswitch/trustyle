55c59abf36ff87e527d708ad9cd7ac9a4f0e7ffc
Joshua Croad <joshcroad@gmail.com>
2020-03-03 10:59:13 +0000

    Update toggle switch to include classnames
    Enables palettes to control the element

--- src/elements/toggle-switch/src/index.tsx
+++ src/elements/toggle-switch/src/index.tsx
@@ -1,8 +1,9 @@
 /** @jsx jsx */
-import { jsx, useThemeUI } from 'theme-ui'
+import { jsx } from 'theme-ui'
 
 interface Props {
   checked: boolean
+  className: string
   onChange?: (e: React.SyntheticEvent) => void
   'aria-label'?: string
   icons?: {
@@ -15,55 +16,62 @@ const ToggleSwitch: React.FC<Props> = ({
   checked,
   onChange,
   icons,
+  className = '',
   ...props
-}) => (
-  <div sx={{ display: 'inline-block' }}>
-    <input
-      checked={checked}
-      onChange={onChange}
-      {...props}
-      sx={{
-        variant: 'srOnly'
-      }}
-      type="checkbox"
-    />
-    <div
-      sx={{
-        width: 80,
-        height: 40,
-        padding: 4,
-        borderWidth: 1,
-        borderStyle: 'solid',
-        borderRadius: '50vw',
-        transition: 'background-color 400ms, border-color 400ms',
-        variant: `toggleSwitch.${checked ? 'checked' : 'unchecked'}`
-      }}
-      role="checkbox"
-      tabIndex={0}
-      aria-checked={checked}
-      aria-label={props['aria-label']}
-      onClick={onChange}
-      onKeyDown={onChange}
-    >
+}) => {
+  const state = checked ? 'checked' : 'unchecked'
+  return (
+    <div sx={{ display: 'inline-block' }} className={`toggle ${className}`}>
+      <input
+        checked={checked}
+        onChange={onChange}
+        {...props}
+        sx={{
+          variant: 'srOnly'
+        }}
+        type="checkbox"
+        className="toggle__input"
+      />
       <div
         sx={{
-          width: 39,
-          height: 38,
+          width: 80,
+          height: 40,
+          padding: 4,
           borderWidth: 1,
           borderStyle: 'solid',
           borderRadius: '50vw',
-          transition: 'transform 400ms, border-color 400ms',
-          transform: checked ? 'translateX(40px)' : 'translateX(0)',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          variant: `toggleSwitch.${checked ? 'checked' : 'unchecked'}.knob`
+          transition: 'background-color 400ms, border-color 400ms',
+          variant: `toggleSwitch.${state}`
         }}
+        className={`toggle__container toggle__container--${state}`}
+        role="checkbox"
+        tabIndex={0}
+        aria-checked={checked}
+        aria-label={props['aria-label']}
+        onClick={onChange}
+        onKeyDown={onChange}
       >
-        {checked ? icons?.checked : icons?.unchecked}
+        <div
+          sx={{
+            width: 39,
+            height: 38,
+            borderWidth: 1,
+            borderStyle: 'solid',
+            borderRadius: '50vw',
+            transition: 'transform 400ms, border-color 400ms',
+            transform: checked ? 'translateX(40px)' : 'translateX(0)',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            variant: `toggleSwitch.${state}.switch`
+          }}
+          className={`toggle__switch toggle__switch--${state}`}
+        >
+          {checked ? icons?.checked : icons?.unchecked}
+        </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default ToggleSwitch
