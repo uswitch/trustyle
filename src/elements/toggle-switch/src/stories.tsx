34a49b2d7fec0565c33a0dab933015d2af0ade63
Joshua Croad <joshcroad@gmail.com>
2020-03-04 13:36:20 +0000

    Toggle Switch Element (#316)
    * Add boilerplate for toggle switch
    
    * Start toggle switch component
    
    * Implement brand styled toggle
    
    * Enhance accessibility
    
    * Add aria-label for accessibility
    
    * Update action to fix Firefox issues
    
    * Update package name
    
    * Publish
    
     - @uswitch/trustyle.toggle-switch@1.0.0
     - @uswitch/trustyle.money-theme@0.5.0
     - @uswitch/trustyle.save-on-energy-theme@0.1.0
     - @uswitch/trustyle.uswitch-theme@0.2.0
    
    * Add sr-only variant to theme
    
    * Remove sr-only css
    
    * Add tab index and onKeyDown
    
    * Rename sr-only variant to camel case
    
    * Update toggle switch to include classnames
    
    Enables palettes to control the element
    
    * Add palette util
    
    * Implement palette util
    
    * Implement painting of toggle switch
    
    * Remove unused interface
    
    * Remove unnecessary palette story
    
    * Remove unnecessary peerDependencies in get util
    
    Co-authored-by: Callum Macrae <callum@macr.ae>

---
+++ src/elements/toggle-switch/src/stories.tsx
@@ -0,0 +1,61 @@
+/** @jsx jsx */
+import { Fragment, useState } from 'react'
+import { jsx } from 'theme-ui'
+import { action } from '@storybook/addon-actions'
+import { boolean, color } from '@storybook/addon-knobs'
+import { PaletteProvider } from '@uswitch/trustyle-utils.palette'
+
+import { checkedIcon, uncheckedIcon } from './assets'
+
+import ToggleSwitch from './'
+
+export default {
+  title: 'Elements|Toggle Switch'
+}
+
+const icons = {
+  checked: checkedIcon,
+  unchecked: uncheckedIcon
+}
+
+export const ExampleWithState = () => {
+  const [toggleState, setToggleState] = useState(true)
+
+  const handleOnChange = () => {
+    const newState = !toggleState
+    setToggleState(newState)
+    action('Toggle changed')(newState)
+  }
+
+  const applyPalette = boolean('Apply palette?', false, 'ToggleSwitch')
+
+  return (
+    <Fragment>
+      <PaletteProvider
+        value={{
+          featureColor: applyPalette
+            ? color('featureColor', '#141424', 'ToggleSwitch')
+            : null
+        }}
+      >
+        <ToggleSwitch
+          aria-label="toggle-1"
+          checked={toggleState}
+          onChange={handleOnChange}
+          icons={icons}
+        />
+        <label sx={{ marginLeft: 10 }} htmlFor="toggle-1">
+          {toggleState ? 'On' : 'Off'}
+        </label>
+      </PaletteProvider>
+    </Fragment>
+  )
+}
+
+export const ToggleOn = () => {
+  return <ToggleSwitch checked />
+}
+
+export const ToggleOff = () => {
+  return <ToggleSwitch checked={false} />
+}
