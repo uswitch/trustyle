b386a243373ff373a229020ca5d3607ef9ea04c9
Callum Macrae <a-callum.macrae@rvu.co.uk>
2020-03-06 17:21:16 +0000

    Border between split rows

--- src/elements/rate-table/src/components/cell-base.tsx
+++ src/elements/rate-table/src/components/cell-base.tsx
@@ -6,20 +6,29 @@ import { CellContext } from './row'
 
 const RateTableCellBase: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({
   children,
+  className,
   ...props
 }) => {
-  const { gridColumn, gridRow } = React.useContext(CellContext)
+  const { gridColumn, gridRow, inSplit } = React.useContext(CellContext)
+
+  // @todo what type should this actually be?
+  const sx: any = {
+    // @todo: should cells by flex by default?
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gridColumn,
+    gridRow
+  }
+
+  if (inSplit) {
+    sx['+ .in-split'] = { borderTop: '1px solid', borderTopColor: 'grey-20' }
+  }
 
   return (
     <div
-      sx={{
-        // @todo: should cells by flex by default?
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        gridColumn,
-        gridRow
-      }}
+      sx={sx}
+      className={`${className} ${inSplit ? 'in-split' : 'not-in-split'}`}
       {...props}
     >
       {children}
