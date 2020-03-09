d615c124bd01d75cdde7834663565c73f964a7a6
Daniel Whyte <danwhy@gmail.com>
2020-03-09 10:59:01 +0000

    adds author profile component

---
+++ src/elements/author-profile/src/index.tsx
@@ -0,0 +1,97 @@
+/** @jsx jsx */
+
+import * as React from 'react'
+import { jsx, Styled } from 'theme-ui'
+
+interface Props extends React.HTMLAttributes<HTMLDivElement> {
+  name: string
+  role: string
+  authorImage?: React.ReactElement
+  authorUrl: string
+  bio: string
+  className?: string
+}
+
+const AuthorProfile: React.FC<Props> = ({
+  name,
+  role,
+  authorImage,
+  authorUrl,
+  bio,
+  className = ''
+}) => {
+  return (
+    <div
+      className={className}
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'row',
+        variant: 'authorProfile.main'
+      }}
+    >
+      {authorImage && (
+        <Styled.a
+          href={authorUrl}
+          sx={{
+            variant: 'author.link',
+            flexShrink: 0,
+            height: '87px',
+            width: '87px',
+            marginRight: 'sm',
+            alignSelf: 'baseline'
+          }}
+        >
+          <authorImage.type
+            {...authorImage.props}
+            sx={{
+              maxHeight: '100%',
+              maxWidth: '100%',
+              borderRadius: '50%',
+              variant: 'authorProfile.image'
+            }}
+          />
+        </Styled.a>
+      )}
+
+      <div
+        sx={{
+          paddingY: 'xs'
+        }}
+      >
+        <Styled.h2
+          sx={{
+            marginTop: 0,
+            marginBottom: 'xxs',
+            fontWeight: 'bold',
+            paddingBottom: 0,
+            variant: 'authorProfile.heading'
+          }}
+          as="h2"
+        >
+          <a
+            href={authorUrl}
+            sx={{
+              textDecoration: 'none',
+              variant: 'authorProfile.link'
+            }}
+          >
+            {name}, {role}
+          </a>
+        </Styled.h2>
+        <Styled.p
+          sx={{
+            fontSize: 'sm',
+            marginY: 0,
+            variant: 'authorProfile.text',
+            width: ['100%', '75%', '60%']
+          }}
+        >
+          {bio}
+        </Styled.p>
+      </div>
+    </div>
+  )
+}
+
+export default AuthorProfile
