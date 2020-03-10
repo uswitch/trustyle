f6e7b8aa1ed72ad9a938396b9daff45f0c5e1369
Callum Macrae <a-callum.macrae@rvu.co.uk>
2020-03-10 15:15:10 +0000

    Add automated tests to skeleton component

--- _element-template/src/stories.tsx
+++ _element-template/src/stories.tsx
@@ -3,6 +3,8 @@ import * as React from 'react'
 import { jsx } from 'theme-ui'
 import { text } from '@storybook/addon-knobs'
 
+import AllThemes, { permutationsGenerator } from '../../../utils/all-themes'
+
 import MyComponent from './'
 
 export default {
@@ -12,4 +14,18 @@ export default {
 export const ExampleWithKnobs = () => {
   const someText: string = text('Some text', 'example')
   return <MyComponent someText={someText} />
+}
+
+export const AutomatedTests = () => {
+  const permutations = permutationsGenerator({
+    someText: ['Text one', 'Text two']
+  })
+
+  return (
+    <AllThemes>
+      {permutations.map((p, i) => (
+        <MyComponent someText={p.someText} key={i} />
+      ))}
+    </AllThemes>
+  )
 }
