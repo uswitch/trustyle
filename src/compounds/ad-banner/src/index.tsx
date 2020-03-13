7705193024b3017cd815e5276799f8d95481ac46
Nathan Hynes <nathanmail@hotmail.co.uk>
2020-03-13 16:12:33 +0000

    Further styling to ad banner compound.

--- src/compounds/ad-banner/src/index.tsx
+++ src/compounds/ad-banner/src/index.tsx
@@ -6,77 +6,109 @@ import { Button } from '@uswitch/trustyle.button'
 
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
   title: string
-  content: string
+  content?: React.ReactElement
   usp: string
-  provider: { logo: string; name: string }
+  sponsor: { logo: string; name: string }
 }
 
-const AdBanner: React.FC<Props> = ({ title, content, usp, provider }) => {
+const AdBanner: React.FC<Props> = ({ title, content, usp, sponsor }) => {
   return (
     <div
       sx={{
-        border: '2px solid red',
-        backgroundColor: 'blue'
+        backgroundColor: 'uswitch-navy',
+        color: '#FFFFFF',
+        position: 'relative',
+        p: ['sm', 'md'],
+        paddingTop: ['210px', 'xl']
       }}
     >
       <div
         sx={{
+          top: 'sm',
+          right: 'sm',
+          fontSize: 'sm',
+          position: 'absolute'
+        }}
+      >
+        Sponsored
+      </div>
+      <div
+        sx={{
           display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center'
+          flexDirection: ['column', 'row', 'row'],
+          alignItems: ['center', 'flex-start']
         }}
       >
         <div
           sx={{
-            maxWidth: '148px'
+            maxWidth: '200px',
+            marginBottom: 'sm'
           }}
         >
           <img
-            src={provider.logo}
-            alt={provider.name}
+            src={sponsor.logo}
+            alt={sponsor.name}
             sx={{
               maxHeight: '100%',
               maxWidth: '100%'
             }}
           />
         </div>
-        <h1
+        <div
           sx={{
-            fontFamily: 'heading',
-            fontStyle: 'normal',
-            fontWeight: 'bold',
-            fontSize: ['md', 'xxl'],
-            textAlign: 'center',
-            border: '2px solid green',
-            marginX: 'md'
+            display: 'flex',
+            flexDirection: 'column',
+            flexGrow: 1,
+            alignItems: ['center', 'flex-start'],
+            px: ['0', 'lg']
           }}
         >
-          {title}
-        </h1>
-        <p
+          <h1
+            sx={{
+              fontStyle: 'normal',
+              fontWeight: 'bold',
+              fontSize: ['lg', 'xxl'],
+              marginTop: 0,
+              marginBottom: 'sm',
+              color: '#FFFFFF',
+              lineHeight: 1
+            }}
+          >
+            {title}
+          </h1>
+          <p
+            sx={{
+              fontSize: 'xs',
+              fontWeight: 'bold',
+              marginBottom: 'sm',
+              textAlign: ['center', 'left']
+            }}
+          >
+            {content}
+          </p>
+          <p
+            sx={{
+              fontSize: 'xs',
+              marginY: 0,
+              fontWeight: 'light',
+              marginBottom: ['sm', 0]
+            }}
+          >
+            {usp}
+          </p>
+        </div>
+        <Button
           sx={{
-            fontFamily: 'heading',
-            fontSize: 'xs',
-            marginX: 'md',
-            border: '2px solid green',
-            textAlign: 'center',
-            fontWeight: 'bold'
+            background: '#FFFFFF',
+            flexShrink: 0,
+            paddingY: 'xs',
+            paddingX: 'md',
+            alignSelf: ['center', 'flex-end']
           }}
+          variant="secondary"
         >
-          {content}
-        </p>
-        <p
-          sx={{
-            fontFamily: 'heading',
-            fontSize: 'xs',
-            marginY: 0,
-            textAlign: 'center',
-            fontWeight: 'light'
-          }}
-        >
-          {usp}
-        </p>
-        <Button variant="primary">Learn More</Button>
+          Learn more
+        </Button>
       </div>
     </div>
   )
