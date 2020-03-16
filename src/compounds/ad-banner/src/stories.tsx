19704e7e27e58145e952e49cc70808c099563b31
Nathan Hynes <nathanmail@hotmail.co.uk>
2020-03-16 14:51:06 +0000

    Created badge with icon. Further design changes to Ad with Banner.

--- src/compounds/ad-banner/src/stories.tsx
+++ src/compounds/ad-banner/src/stories.tsx
@@ -20,11 +20,25 @@ export const ExampleWithKnobs = () => {
 
   const url =
     'https://uswitch-mobiles-contentful.imgix.net/kf81nsuntxeb/3OTEiajt8kWEUm8QcWI8w8/aab828afa135b4591375dc71d6a6584d/Logo_-_samsung.png?auto=compress%2Cformat&fit=fill&trim=color&ixlib=react-8.6.4&invert=true&sat=-100&con=100'
+
   const usp: string = text('usp', 'Free Galaxy Buds+ worth £159')
   const sponsorLogo: string = text('Sponsor Logo', url)
   const sponsorName: string = text('Sponsor Name', 'Samsung')
+  const label: string = text('Badge Label', 'Uswitch Manufacturer of the Year')
+  const backgroundImage: string = text(
+    'background Image',
+    'https://bit.ly/2TTd2SZ'
+  )
+
   const sponsor = { logo: sponsorLogo, name: sponsorName }
   return (
-    <AdBanner title={title} content={content} usp={usp} sponsor={sponsor} />
+    <AdBanner
+      title={title}
+      content={content}
+      usp={usp}
+      sponsor={sponsor}
+      label={label}
+      backgroundImage={backgroundImage}
+    />
   )
 }
