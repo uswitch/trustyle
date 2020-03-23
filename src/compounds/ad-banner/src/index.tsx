11685b35347c9f03f2e61dfaa83d07195344f889
Callum Macrae <a-callum.macrae@rvu.co.uk>
2020-03-23 16:44:56 +0000

    Revert breaking change to button component

--- src/compounds/ad-banner/src/index.tsx
+++ src/compounds/ad-banner/src/index.tsx
@@ -92,8 +92,16 @@ const AdBanner: React.FC<Props> = ({
           }}
         >
           <Badge variant={badgeVariant}>
-            {badgeIcon}
-            {label}
+            <div
+              sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center'
+              }}
+            >
+              {badgeIcon}
+              {label}
+            </div>
           </Badge>
         </div>
         <div
