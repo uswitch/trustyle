96d77f686a061fbf13ed8ecc2d7a96ee7f9dcd35
Barney Fox <barneyfoxuk@gmail.com>
2020-03-26 19:04:58 +0000

    successfully working loading eevees packages

--- src/utils/theme-composer/src/index.ts
+++ src/utils/theme-composer/src/index.ts
@@ -4,17 +4,12 @@
 import moneyTokens from '@uswitch/trustyle.money-theme'
 import uswitchTokens from '@uswitch/trustyle.uswitch-theme'
 
-// Elements
-import { multibrandOptions as badge } from '@uswitch/trustyle.badge'
-
 const tokens: any = {
   money: moneyTokens,
   uswitch: uswitchTokens
 }
 
-const elements: any = [
-  '@uswitch/trustyle.badge'
-]
+const elements: any = ['@uswitch/trustyle.badge']
 
 const composeTheme = (
   brand: string,
@@ -24,32 +19,44 @@ const composeTheme = (
 ) => {
   const dependencies = packageJson[dependencyType]
 
-  const dependencyNames = 
-    Object.keys(dependencies)
-      .filter(dep => dep.match('@uswitch/trustyle'))
-      .filter(dep => !dep.includes('theme'))
+  const dependencyNames = Object.keys(dependencies)
+    .filter(dep => dep.match('@uswitch/trustyle'))
+    .filter(dep => !dep.includes('-utils'))
+    .filter(dep => !dep.includes('theme'))
 
-  const componentThemes = dependencyNames.reduce(
-    (obj, dependancy) => ({ 
-      ...obj, 
-      [dependancy]: require(`${nodeModulesPath}/${dependancy}/lib/themes/${brand}.js`).default
-    }),
-    {}
-  )
+  const componentThemes = dependencyNames.reduce((obj, dependancy) => {
+    try {
+      return {
+        ...obj,
+        [dependancy]: require(`${nodeModulesPath}/${dependancy}/lib/themes/${brand}.js`)
+          .default
+      }
+    } catch (e) {
+      return obj
+    }
+  }, {})
 
+  console.log('componentThemes', componentThemes)
 
   const theme = {
     ...tokens[brand],
     '----------------------------- Variants': '----------------------------- ',
-    elements: 
-      Object.keys(componentThemes)
-        .filter(name => elements.includes(name))
-        .reduce((obj, packageName) => {
-          return {
-            ...obj,
-            ...{ [console.log('name', packageName.replace('@uswitch/trustyle.', '')) || packageName.replace('@uswitch/trustyle.', '')]: componentThemes[packageName] }
+    elements: Object.keys(componentThemes)
+      .filter(name => elements.includes(name))
+      .reduce((obj, packageName) => {
+        return {
+          ...obj,
+          ...{
+            [console.log(
+              'name',
+              packageName.replace('@uswitch/trustyle.', '')
+            ) ||
+            packageName.replace('@uswitch/trustyle.', '')]: componentThemes[
+              packageName
+            ]
           }
-        }, {}),
+        }
+      }, {}),
     compounds: {}
   }
 
