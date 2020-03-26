df520aa35acb903cda69d3ba14ec90978602a1df
Barney Fox <barneyfoxuk@gmail.com>
2020-03-26 19:07:39 +0000

    latest theme builder

---
+++ src/elements/badge/src/themes/money.ts
@@ -0,0 +1,18 @@
+const theme: any = {
+  base: {
+    paddingX: 'base',
+    paddingY: [3, 5.5],
+    fontSize: ['xs', 'sm'],
+    fontWeight: 'bold',
+    borderRadius: '50vh'
+  },
+  variants: {
+    primary: {
+      variant: 'badge.base',
+      backgroundColor: 'plum',
+      color: 'white'
+    }
+  }
+}
+
+export default theme
