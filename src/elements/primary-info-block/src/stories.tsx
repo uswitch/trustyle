c02bb4bbed59783354e10811a1db7648f780b200
natyeo <natalie.yeo@rvu.co.uk>
2020-03-31 14:50:31 +0100

    Sponsored product compound (#384)
    * Add primary info block element
    
    * Create usp tag element
    
    * Change font family
    
    * Add more usp examples
    
    * Build mobile ad compound using buttonlink, primaryinfotag and usptag elements
    
    * Refactor standard p0 ad compound to use Imgix and flex-grid
    
    * If no usp then no usp tag rendered
    
    * Rename compound standard p0 ad
    
    * If value is 'unlimited', make small
    
    * Create sponsored-by-tag element
    
    * Create Awards Tag element
    
    * Add sponsored by, award tag and enhanced image to p0 ad
    
    * Rename compound and fix Typescript errors
    
    * Make compound responsive to ipad & desktop and add box shadow
    
    * Add badge element to compound
    
    * Complete desktop hero version of compound
    
    * Change name of compound to Sponsored product
    
    * Publish
    
     - @uswitch/trustyle.sponsored-product@0.0.10
     - @uswitch/trustyle.awards-tag@0.0.1
     - @uswitch/trustyle.primary-info-block@0.2.3
     - @uswitch/trustyle.sponsored-by-tag@0.0.1
     - @uswitch/trustyle.usp-tag@0.0.1
     - @uswitch/trustyle.uswitch-theme@0.8.5

---
+++ src/elements/primary-info-block/src/stories.tsx
@@ -0,0 +1,55 @@
+/** @jsx jsx */
+
+import { css, jsx } from '@emotion/core'
+import { text } from '@storybook/addon-knobs'
+
+import PrimaryInfoBlock from './'
+
+export default {
+  title: 'Elements|Primary Info Block'
+}
+
+export const Example = () => {
+  const values = [
+    {
+      value: '10',
+      suffix: 'GB',
+      label: 'data'
+    },
+    {
+      value: 'unlimited',
+      label: 'data'
+    },
+    {
+      value: '10.00',
+      prefix: '£',
+      label: 'per month'
+    }
+  ]
+
+  const allVariants = values.map((val, i) => (
+    <div css={css({ margin: '16px 0' })} key={i}>
+      <PrimaryInfoBlock
+        prefix={val.prefix}
+        value={val.value}
+        suffix={val.suffix}
+        label={val.label}
+      />
+    </div>
+  ))
+
+  return (
+    <div>
+      <PrimaryInfoBlock
+        prefix={text('prefix', '£')}
+        value={text('value', '10.00')}
+        suffix={text('suffix', '')}
+        label={text('label', 'per month')}
+      />
+      <div>
+        <h3>With stubbed data:</h3>
+        {allVariants}
+      </div>
+    </div>
+  )
+}
---
+++ src/elements/usp-tag/src/index.tsx
@@ -0,0 +1,42 @@
+/** @jsx jsx */
+
+import * as React from 'react'
+import { jsx } from 'theme-ui'
+
+interface Props extends React.HTMLAttributes<HTMLDivElement> {
+  usp?: string
+}
+
+const UspTag: React.FC<Props> = ({ usp }) =>
+  usp ? (
+    <div
+      sx={{
+        minHeight: 32,
+        backgroundColor: 'yellow-25',
+        display: 'flex',
+        alignItems: 'center',
+        '::before': {
+          content: '""',
+          display: 'inline-block',
+          width: 4,
+          minHeight: 28,
+          margin: 2,
+          backgroundColor: 'yellow'
+        }
+      }}
+    >
+      <span
+        sx={{
+          fontFamily: "'-apple-system', 'BlinkMacSystemFont'",
+          fontWeight: 'bold',
+          fontSize: ['xs', 11],
+          color: 'grey-80',
+          marginLeft: 'xs'
+        }}
+      >
+        {usp}
+      </span>
+    </div>
+  ) : null
+
+export default UspTag
