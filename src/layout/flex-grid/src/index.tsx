69284210e033c3c1dd3b5746703d3425b5139285
domtronn <dgc336@gmail.com>
2020-03-31 11:05:08 +0100

    Overload get methods to work as expected an allow arrays of gutters etc

--- src/layout/flex-grid/src/index.tsx
+++ src/layout/flex-grid/src/index.tsx
@@ -3,18 +3,47 @@
 import * as React from 'react'
 import { jsx } from 'theme-ui'
 
-const getSpaceValue = (key: string) => (theme: any = {}): number =>
-  theme.space[key]
+function getSpaceValue(
+  key: string | string[] | number | number[]
+): (theme: object) => number[]
+function getSpaceValue(
+  key: string | string[] | number | number[],
+  index?: number
+): (theme: object) => number
+function getSpaceValue(key: any, index?: any) {
+  return (theme: any = {}): any => {
+    const spaceValues = ([] as any[])
+      .concat(key)
+      .map(it => (typeof it === 'number' ? it : theme.space[it] || 0))
 
-const getNegativeSpaceValue = (key: string) => (theme: any = {}): string =>
-  `-${getSpaceValue(key)(theme)}px`
+    return index ? spaceValues[index] || spaceValues[0] : spaceValues || 0
+  }
+}
 
-const getGutterSize = (theme: any): string => theme?.grid?.sizes?.gutter
+function getNegativeSpaceValue(
+  key: string | string[] | number | number[]
+): (theme: object) => string[]
+function getNegativeSpaceValue(
+  key: string | string[] | number | number[],
+  index?: number
+): (theme: object) => string
+function getNegativeSpaceValue(key: any, index?: any) {
+  return (theme: any = {}): any => {
+    const spaceValues = ([] as number[])
+      .concat(getSpaceValue(key)(theme))
+      .map(it => `-${it}px`)
+
+    return index ? spaceValues[index] || spaceValues[0] : spaceValues || 0
+  }
+}
+
+const getGutterSize = (theme: any): string | string[] | number | number[] =>
+  theme?.grid?.sizes?.gutter
 
 const getVerticalGutterSize = (theme: any): string | number =>
   theme?.grid?.sizes?.verticalGutter
 
-const getContainerSize = (theme: any): number =>
+const getContainerSize = (theme: any): number | number[] =>
   theme?.grid?.container?.maxWidth
 
 const mediaQueryFunction = (
@@ -46,13 +75,7 @@ export const Container: React.FC<ContainerProps> = ({
         maxWidth: (theme: any) =>
           ([] as number[])
             .concat(getContainerSize(theme))
-            .map(
-              maxWidth =>
-                `calc(${maxWidth *
-                  (cols && span ? span / cols : 1)}px - ${getSpaceValue('sm')(
-                  theme
-                )}px)`
-            )
+            .map(maxWidth => maxWidth * (cols && span ? span / cols : 1))
       }}
       {...props}
     >
@@ -79,7 +102,8 @@ export const Row: React.FC<RowProps> = ({
     <div
       sx={{
         variant: `grid.row`,
-        mx: (theme: any): any => getNegativeSpaceValue(getGutterSize(theme)),
+        mx: (theme: any): any =>
+          getNegativeSpaceValue(getGutterSize(theme))(theme),
         display: 'flex',
         flexDirection: direction,
         flexWrap: wrap ? 'wrap' : 'nowrap',
@@ -134,8 +158,10 @@ export const Col: React.FC<ColProps> = ({
                 mediaQueryFunction(cols, (colValue, index) => {
                   return `calc(${((Array.isArray(span) ? span[index] : span) /
                     colValue) *
-                    100}% - ${getSpaceValue(getGutterSize(theme))(theme) *
-                    2}px)`
+                    100}% - ${getSpaceValue(
+                    getGutterSize(theme),
+                    index
+                  )(theme) * 2}px)`
                 })
             }
           : {}),
@@ -147,7 +173,10 @@ export const Col: React.FC<ColProps> = ({
                     ? offset[index]
                     : offset) /
                     colValue) *
-                    100}% + ${getSpaceValue(getGutterSize(theme))(theme)}px)`
+                    100}% + ${getSpaceValue(
+                    getGutterSize(theme),
+                    index
+                  )(theme)}px)`
                 })
             }
           : { ml: getGutterSize })
