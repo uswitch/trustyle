f689dfb8464042ac81e8a9491e7c0ea8dcc79d19
Nathan Hynes <nathanmail@hotmail.co.uk>
2020-05-05 15:07:56 +0100

    Change to story.

--- src/elements/call-out/src/stories.tsx
+++ src/elements/call-out/src/stories.tsx
@@ -16,7 +16,7 @@ export const Example = () => {
   const titleText = text(`Call out title`, `This is a call out`)
   const textText = text(
     `Call out text`,
-    `Tempor incididunt ut labore et dolore magna aliqua. Sint id est dolor laborum. Sed do eiusmod tempor incididunt ut labore. Lorem ipsum dolores est unt.`
+    `Tempor incididunt ut labore et dolore magna aliqua. Sint id est dolor laborum. Sed do eiusmod tempor incididunt ut labore. Lorem ipsum dolores est unt. `
   )
   const showIcon = boolean('Show icon?', true)
   const iconGlyph = select(
