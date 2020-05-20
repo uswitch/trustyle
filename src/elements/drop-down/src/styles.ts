ca5f7922bbfe7f9709ee8c08e1d05b31c12d65d5
Peter Horne <peter.horne@uswitch.com>
2020-05-20 15:19:57 +0100

    feat: themeable drop-down

--- src/elements/drop-down/src/styles.ts
+++ src/elements/drop-down/src/styles.ts
@@ -1,46 +1,54 @@
-import { css, SerializedStyles } from '@emotion/core'
-import {
-  colors,
-  inputs,
-  pxToRem,
-  spacers,
-  typography
-} from '@uswitch/trustyle.styles'
+import { colors, pxToRem, spacers } from '@uswitch/trustyle.styles'
+import { SxStyleProp } from 'theme-ui'
 
-export const icon: SerializedStyles = css({
+const { blueGrey, tomato, UswitchNavy } = colors
+
+export const container: SxStyleProp = {
+  position: 'relative'
+}
+
+export const select = (hasError: boolean, hasFocus: boolean): SxStyleProp => ({
+  fontFamily: 'base',
+  fontSize: 'base',
+  lineHeight: '1.33',
+  color: 'text',
+  appearance: 'none',
+  backgroundColor: colors.white,
+  boxShadow: (theme: any) =>
+    hasError
+      ? `inset 0 0 0 1px ${theme.elements?.['drop-down']?.errorColor ?? tomato}`
+      : hasFocus
+      ? `inset 0 0 0 1px ${theme.elements?.['drop-down']?.focusColor ??
+          UswitchNavy}`
+      : null,
+  borderWidth: 1,
+  borderStyle: 'solid',
+  borderColor: (theme: any) =>
+    hasError
+      ? theme.elements?.['drop-down']?.errorColor ?? tomato
+      : hasFocus
+      ? theme.elements?.['drop-down']?.focusColor ?? UswitchNavy
+      : theme.elements?.['drop-down']?.defaultColor ?? blueGrey,
+  borderRadius: '3px',
+  boxSizing: 'border-box',
+  outline: 'none', // remove Chrome mobile default focus
+  padding: pxToRem(16, spacers.orange, 16, 16),
+  verticalAlign: 'middle',
+  width: '100%',
+  '&:-moz-focusring': {
+    color: 'transparent',
+    textShadow: `0 0 0 ${colors.black}`
+  },
+  '&::-ms-expand': {
+    display: 'none'
+  }
+})
+
+export const icon: SxStyleProp = {
   height: '15px',
   width: '15px',
   pointerEvents: 'none',
   position: 'absolute',
   right: pxToRem(16),
   top: 'calc(50% - 15px / 2)'
-})
-
-export const root = (hasError: boolean, hasFocus: boolean): SerializedStyles =>
-  css([
-    typography.input,
-    {
-      appearance: 'none',
-      backgroundColor: colors.white,
-      border: `solid 1px ${colors.lightGreyBlue}`,
-      borderRadius: '3px',
-      boxSizing: 'border-box',
-      color: colors.black,
-      outline: 'none', // remove Chrome mobile default focus
-      padding: pxToRem(16, spacers.orange, 16, 16),
-      verticalAlign: 'middle',
-      width: '100%',
-      '&:-moz-focusring': {
-        color: 'transparent',
-        textShadow: `0 0 0 ${colors.black}`
-      },
-      '&::-ms-expand': {
-        display: 'none'
-      }
-    },
-    inputs.emphasis(hasError, hasFocus)
-  ])
-
-export const container = css({
-  position: 'relative'
-})
+}
