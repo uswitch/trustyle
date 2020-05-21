67e9dff0402a0fca073a8412617c5aa2c7bc19e0
Phil Helm <github@phelm.co.uk>
2020-05-21 17:48:10 +0100

    margin TOP

--- src/elements/category/src/index.tsx
+++ src/elements/category/src/index.tsx
@@ -68,7 +68,7 @@ const Category: React.FC<ListProps> = ({
             )}
           </Col>
           {Image ? (
-            <Col span={[12, 6]} sx={{ marginBottom: ['sm', 0] }}>
+            <Col span={[12, 6]} sx={{ marginTop: ['sm', 0] }}>
               {Image}
             </Col>
           ) : null}
