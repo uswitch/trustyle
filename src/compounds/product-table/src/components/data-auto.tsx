0d7233b392365bb458a7a54eaadf20e21f7b3458
Callum Macrae <a-callum.macrae@rvu.co.uk>
2020-05-21 15:58:40 +0100

    Fix (valid) typescript errors

--- src/compounds/product-table/src/components/data-auto.tsx
+++ src/compounds/product-table/src/components/data-auto.tsx
@@ -2,20 +2,31 @@
 import * as React from 'react'
 import { jsx } from 'theme-ui'
 
+interface FormattedText {
+  size: 'big' | 'small'
+  word: string
+}
+
 /**
  * Attempts to automatically format data. Bit of a hack - use formatted data
  * where possible.
  *
  * Doesn't work with all data, test before using.
  */
-function autoFormat(text: string) {
+function autoFormat(text?: string): FormattedText[] {
+  if (typeof text !== 'string') {
+    return [{ size: 'small', word: '-' }]
+  }
+
   return text
     .replace(/\b\//g, ' /')
     .split(' ')
-    .map(word => ({
-      word: word,
-      size: /\d/.test(word) || word === '-' ? 'big' : 'small'
-    }))
+    .map(
+      (word): FormattedText => ({
+        word,
+        size: /\d/.test(word) || word === '-' ? 'big' : 'small'
+      })
+    )
     .reduce((words, word, i) => {
       if (i !== 0 && word.size === words[words.length - 1].size) {
         words[words.length - 1].word += ` ${word.word}`
@@ -23,7 +34,7 @@ function autoFormat(text: string) {
       }
 
       return words.concat(word)
-    }, [])
+    }, [] as FormattedText[])
 }
 
 export interface DataAutoProps extends React.HTMLAttributes<HTMLDivElement> {
