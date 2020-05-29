81fef37aaf84a490b0bcead64c2006ffba64cf9c
Brendon Boshell <brendon.boshell@uswitch.com>
2020-05-29 14:26:16 +0100

    Remove unused script

--- scripts/updatepackage.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for path in $(npx lerna list --json | jq -r 'map(.location) | .[]'); do
-  # cat "$path/package.json" | jq 'del(.scripts)' > tmp.json && mv tmp.json "$path/package.json"
-  rm "$path/tsconfig.json"
-done
