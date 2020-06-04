139c2b8b98181ab5e04dbfcc3b1ed225edd79dbc
meghaniankov <meghan.e.mccue@gmail.com>
2020-06-04 15:11:33 +0100

    feat: re-wrote scrollable login within tabs component

--- src/compounds/tabs/src/index.tsx
+++ src/compounds/tabs/src/index.tsx
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-import React, { useState } from 'react'
+import React, { RefObject, useEffect, useState } from 'react'
 import { jsx, Styled } from 'theme-ui'
 import { Col, Container, Row } from '@uswitch/trustyle.flex-grid'
 
@@ -62,32 +62,100 @@ interface TabsProps extends React.HTMLAttributes<HTMLDivElement> {
 
 export const Tabs: React.FC<TabsProps> = ({ children }) => {
   const [activeTab, setActiveTab] = useState(0)
+  const [left, setLeft] = useState(0)
+  const tabs: RefObject<HTMLDivElement> = React.createRef()
+  const tabWrap: RefObject<HTMLDivElement> = React.createRef()
+  const [padding, setPadding] = useState(0)
+  const [width, setWidth] = useState(0)
+  const [wrapWidth, setWrapWidth] = useState(0)
+  const [mouseIsDown, setMouseIsDown] = useState(false)
+  const [lastX, setLastX] = useState(false)
+
+  useEffect(() => {
+    setPadding(tabs?.current?.offsetHeight)
+    setWidth(tabs?.current?.offsetWidth)
+  }, [tabs])
+
+  useEffect(() => {
+    setWrapWidth(tabWrap?.current?.offsetWidth)
+  }, [tabWrap])
+
+  function setRealLeft(left: number) {
+    if (left > 0) {
+      return setLeft(0)
+    }
+
+    const minLeft = -(width - wrapWidth)
+
+    if (left < minLeft) {
+      return setLeft(minLeft)
+    }
+
+    setLeft(left)
+  }
+
+  function onMouseDown(event) {
+    if (wrapWidth > width) return
+    setLastX(event.clientX)
+    setMouseIsDown(true)
+  }
+
+  function onMouseUp() {
+    setMouseIsDown(false)
+  }
+
+  function onMouseMove(event) {
+    if (!mouseIsDown) return
+
+    if (lastX > event.clientX) {
+      const diff = lastX - event.clientX
+      setRealLeft(left - diff)
+    } else {
+      const diff = event.clientX - lastX
+      setRealLeft(left + diff)
+    }
+
+    setLastX(event.clientX)
+  }
+
+  window.addEventListener('mouseup', onMouseUp)
 
   return (
     <Container>
-      <Row
-        direction="row"
-        wrap={false}
+      <div
+        ref={tabWrap}
         sx={{
-          overflowX: 'scroll',
-          '::-webkit-scrollbar': { display: 'none' },
-          '-ms-overflow-style': 'none'
+          overflowX: 'hidden',
+          paddingTop: `${padding}px`,
+          position: 'relative'
         }}
       >
-        {React.Children.map(children, (child, index) => (
-          <Col sx={{ mx: 'md', mb: 0 }}>
-            <TabLink
-              key={index}
-              title={child.props.title}
-              onClick={e => {
-                e.preventDefault()
-                setActiveTab(index)
-              }}
-              isActive={index === activeTab}
-            />
-          </Col>
-        ))}
-      </Row>
+        <div
+          ref={tabs}
+          onMouseMove={onMouseMove}
+          onMouseDown={onMouseDown}
+          sx={{
+            position: 'absolute',
+            left: `${left}px`,
+            top: 0
+          }}
+        >
+          <Row direction="row" wrap={false}>
+            {React.Children.map(children, (child, index) => (
+              <Col sx={{ mx: 'md', mb: 0 }} key={index}>
+                <TabLink
+                  title={child.props.title}
+                  onClick={e => {
+                    e.preventDefault()
+                    setActiveTab(index)
+                  }}
+                  isActive={index === activeTab}
+                />
+              </Col>
+            ))}
+          </Row>
+        </div>
+      </div>
       <Row cols={12}>
         <Col span={12}>
           {React.Children.map(children, (child, index) => (
