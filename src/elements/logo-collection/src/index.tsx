e44d84b948b82087a65fbffe7f43614a08b28864
Sam Egger <segger87@gmail.com>
2020-06-09 10:59:19 +0100

    "feat: create provider logo element"

---
+++ src/elements/logo-collection/src/index.tsx
@@ -0,0 +1,32 @@
+/** @jsx jsx */
+
+import * as React from 'react'
+import { jsx } from 'theme-ui'
+
+interface Props extends React.HTMLAttributes<HTMLDivElement> {
+  providerImage: React.ReactElement[]
+}
+
+const LogoCollection: React.FC<Props> = ({ providerImage }) => {
+  return (
+    <div
+      sx={{
+        display: 'flex',
+        flexWrap: 'wrap'
+      }}
+    >
+      {providerImage.map((img, i) => (
+        <div
+          key={`logo-collection-${i}`}
+          sx={{
+            mr: '16px'
+          }}
+        >
+          {img}
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default LogoCollection
