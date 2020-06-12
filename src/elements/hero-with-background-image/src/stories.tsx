2db0ad824e228ac7ac666a995be33b3393c59667
Liam McCombes <liam.mccombes@uswitch.com>
2020-06-12 10:24:15 +0100

    Add breadcrumbs and content to story

--- src/elements/hero-with-background-image/src/stories.tsx
+++ src/elements/hero-with-background-image/src/stories.tsx
@@ -1,31 +1,120 @@
 /** @jsx jsx */
 import * as React from 'react'
-import { jsx } from 'theme-ui'
+import { jsx, Styled } from 'theme-ui'
 
 import AllThemes from '../../../utils/all-themes'
-import { Container } from '../../../layout/flex-grid/src'
+import { Col, Container, Row } from '../../../layout/flex-grid/src'
+import { Button } from '../../button/src'
+import Breadcrumbs from '../../breadcrumbs/src'
+import IconTile from '../../icon-tile/src'
 
 import HeroWithBackgroundImage from './'
+
+const images = {
+  wallet: require('../../../../static/hero-with-background-image/wallet.png')
+}
 
 export default {
   title: 'Elements|HeroWithBackgroundImage'
 }
 
-const imageUrl = require('../../../../static/hero-with-background-image/wallet.png')
-
-export const ExampleWithKnobs = () => {
-  return <HeroWithBackgroundImage imageUrl={imageUrl} />
-}
-
-ExampleWithKnobs.story = {
-  parameters: {
-    percy: { skip: true }
+const crumbs = [
+  {
+    fields: {
+      path: '/',
+      displayText: 'Home'
+    }
+  },
+  {
+    fields: {
+      path: '/gas-electricity/',
+      displayText: 'Gas & Electricity'
+    }
+  },
+  {
+    fields: {
+      path: '/gas-electricity/guides',
+      displayText: 'Guides'
+    }
   }
+]
+
+const breadcrumbs = (
+  <Breadcrumbs
+    crumbs={crumbs}
+    title="Understanding energy bills and electricity bills - FAQs and more"
+    variant="light"
+  />
+)
+
+interface PanelHelperProps extends React.HTMLAttributes<any> {
+  icon: string
 }
+const IconPanelHelper: React.FC<PanelHelperProps> = ({ icon, children }) => {
+  const iconImg = (
+    <img src={require(`../../../../static/money-icons/${icon}.svg`)} />
+  )
+  return <IconTile icon={iconImg}>{children}</IconTile>
+}
+
+const content = (
+  <React.Fragment>
+    <Row>
+      <Col span={[12, 6]}>
+        <Styled.h1 sx={{ marginTop: 0 }}>
+          Get a headstart on your savings goals and take out an investment ISA
+        </Styled.h1>
+        <Button variant="primary">Compare investment ISAs</Button>
+      </Col>
+    </Row>
+    <Styled.h2>Compare and find the deal that&apos;s right for you</Styled.h2>
+    <Row wrap direction="row">
+      <Col span={[6, 3]}>
+        <IconPanelHelper icon="creditCards">Credit cards</IconPanelHelper>
+      </Col>
+      <Col span={[6, 3]}>
+        <IconPanelHelper icon="mortgages">Mortgages</IconPanelHelper>
+      </Col>
+      <Col span={[6, 3]}>
+        <IconPanelHelper icon="loans">Loans</IconPanelHelper>
+      </Col>
+      <Col span={[6, 3]}>
+        <IconPanelHelper icon="savingAccounts">
+          Savings accounts
+        </IconPanelHelper>
+      </Col>
+      <Col span={[6, 3]}>
+        <IconPanelHelper icon="travelMoney">Travel money</IconPanelHelper>
+      </Col>
+      <Col span={[6, 3]}>
+        <IconPanelHelper icon="currentAccounts">
+          Current accounts
+        </IconPanelHelper>
+      </Col>
+      <Col span={[6, 3]}>
+        <IconPanelHelper icon="carInsurance">Car insurance</IconPanelHelper>
+      </Col>
+      <Col span={[6, 3]}>
+        <IconPanelHelper icon="investments">Investments</IconPanelHelper>
+      </Col>
+    </Row>
+  </React.Fragment>
+)
 
 export const HeroWithCustomContainer = () => {
-  const Container = () => <div sx={{ width: 100, height: 100, margin: 'auto' }}></div>
-  return <HeroWithBackgroundImage imageUrl={imageUrl} container={Container} />
+  const Container: React.FC = ({ children }) => (
+    <div sx={{ width: 600, margin: 'auto' }}>{children}</div>
+  )
+
+  return (
+    <HeroWithBackgroundImage
+      imageUrl={images.wallet}
+      container={Container}
+      breadcrumbs={breadcrumbs}
+    >
+      {content}
+    </HeroWithBackgroundImage>
+  )
 }
 
 HeroWithCustomContainer.story = {
@@ -35,8 +124,15 @@ HeroWithCustomContainer.story = {
 }
 
 export const HeroWithFlexGridContainer = () => {
-  const Foo = () => <Container sx={{ height: 500 }} />
-  return <HeroWithBackgroundImage imageUrl={imageUrl} container={Foo} />
+  return (
+    <HeroWithBackgroundImage
+      imageUrl={images.wallet}
+      container={Container}
+      breadcrumbs={breadcrumbs}
+    >
+      {content}
+    </HeroWithBackgroundImage>
+  )
 }
 
 HeroWithFlexGridContainer.story = {
@@ -48,7 +144,7 @@ HeroWithFlexGridContainer.story = {
 export const AutomatedTests = () => {
   return (
     <AllThemes>
-      <HeroWithBackgroundImage/>
+      <HeroWithBackgroundImage />
     </AllThemes>
   )
 }
