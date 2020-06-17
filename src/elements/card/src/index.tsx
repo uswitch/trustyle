cba9da03c3fd2dd59af95295e343176032e09ab8
Ruxandra Anghel <ruxandra.anghel@rvu.co.uk>
2020-06-17 10:09:36 +0100

    Change prop name from date to metaInfo

--- src/elements/card/src/index.tsx
+++ src/elements/card/src/index.tsx
@@ -6,7 +6,7 @@ import { ImgixImage } from '@uswitch/trustyle.imgix-image'
 
 interface Props {
   className?: string
-  date?: string
+  metaInfo?: string
   description?: string
   horizontal?: boolean
   imageSize?: 'cover' | 'contain'
@@ -24,7 +24,7 @@ const makeStyles = (variant: string) => (element?: string) =>
 
 const Card: React.FC<Props> = ({
   className = '',
-  date,
+  metaInfo,
   description,
   horizontal = false,
   imageSize = 'cover',
@@ -79,7 +79,9 @@ const Card: React.FC<Props> = ({
               {tag}
             </span>
           )}
-          {date && <Styled.p sx={{ variant: styles('date') }}>{date}</Styled.p>}
+          {metaInfo && (
+            <Styled.p sx={{ variant: styles('metaInfo') }}>{metaInfo}</Styled.p>
+          )}
         </div>
 
         <Styled.h3 sx={{ margin: '0' }}>
