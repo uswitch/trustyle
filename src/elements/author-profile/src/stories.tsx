7cf9ec2883c06fcda9d87d0f38c2a6e5eebc6f1e
Antonio Kamber <antonio.kamber@q.agency>
2020-06-18 12:56:17 +0200

    Added details option example to stories

--- src/elements/author-profile/src/stories.tsx
+++ src/elements/author-profile/src/stories.tsx
@@ -42,6 +42,38 @@ ExampleWithKnobs.story = {
   }
 }
 
+export const ExampleWithDetails = () => {
+  const name = text('Author name', 'Salman')
+  const email = text('Author email', 'salman@gmail.com')
+  const role = text('Author role', 'Job title')
+  const authorUrl = text('Author URL', 'https://www.jonathanfielding.com/')
+  const image = (
+    <img src="https://placekitten.com/200/200?image=9" alt="Salman" />
+  )
+  const bio = text(
+    'Author Biography',
+    'This section is used to talk about the author. Here you can explain their background, what they do. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.'
+  )
+
+  return (
+    <AuthorProfile
+      name={name}
+      role={role}
+      authorImage={image}
+      authorUrl={authorUrl}
+      bio={bio}
+      email={email}
+      detailed
+    />
+  )
+}
+
+ExampleWithDetails.story = {
+  parameters: {
+    percy: { skip: true }
+  }
+}
+
 export const AutomatedTests = () => {
   return (
     <AllThemes themes={['uswitch', 'money']}>
