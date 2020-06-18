3179dd16f0a265260f60210448487a971d704eb9
Liam McCombes <liam.mccombes@uswitch.com>
2020-06-18 17:00:54 +0100

    Begin theming for cupcard

--- src/elements/read-more-card/src/index.tsx
+++ src/elements/read-more-card/src/index.tsx
@@ -1,5 +1,4 @@
 /** @jsx jsx */
-
 import * as React from 'react'
 import { jsx } from 'theme-ui'
 import { Icon } from '@uswitch/trustyle.icon'
@@ -8,36 +7,89 @@ interface Props extends React.HTMLAttributes<HTMLDivElement> {
   text: string
   href: string
   className?: string
+  variant?: string
 }
 
-const outerElementSx = {
-  variant: 'styles.h4',
+const baseStyle = {
+  outer: {
+    variant: 'styles.h4',
 
-  display: 'flex',
-  alignItems: 'center',
-  borderWidth: 0,
-  borderLeftWidth: 6,
-  borderStyle: 'solid',
-  borderColor: 'primary',
-  textDecoration: 'none',
-  transition: 'all 0.3s ease',
-  '&:hover': {
-    textDecoration: 'underline',
-    color: 'anchor-hover',
-    backgroundColor: 'grey-10'
+    display: 'flex',
+    textDecoration: 'none',
+    transition: 'all 0.3s ease',
+    backgroundColor: 'grey-05'
   },
-  paddingLeft: 'md',
-  paddingRight: 'sm',
-  paddingTop: 'sm',
-  paddingBottom: 'base',
-  backgroundColor: 'grey-05'
+  text: {
+    flexGrow: 1
+  },
+  icon: {
+    flexShrink: 0,
+    paddingLeft: 'md'
+  }
 }
 
-const ReadMoreCard: React.FC<Props> = ({ text, href, className }) => {
+const variantStyles = {
+  default: {
+    outer: {
+      ...baseStyle.outer,
+
+      alignItems: 'center',
+      borderWidth: 0,
+      borderLeftWidth: 6,
+      borderStyle: 'solid',
+      borderColor: 'primary',
+      paddingLeft: 'md',
+      paddingRight: 'sm',
+      paddingTop: 'sm',
+      paddingBottom: 'base',
+      '&:hover': {
+        textDecoration: 'underline',
+        color: 'anchor-hover',
+        backgroundColor: 'grey-10'
+      }
+    },
+    text: {
+      ...baseStyle.text
+    },
+    icon: {
+      ...baseStyle.icon
+    }
+  },
+  cupcard: {
+    outer: {
+      ...baseStyle.outer,
+      padding: 'md',
+      flexDirection: 'column',
+      alignItems: 'stretch',
+      height: 176,
+      '&:hover': {
+        boxShadow: 'card',
+        opacity: 1
+      }
+    },
+    text: {
+      ...baseStyle.text,
+      marginBottom: 'md'
+    },
+    icon: {
+      ...baseStyle.icon,
+      alignSelf: 'flex-end'
+    }
+  }
+}
+
+const getStyle = (variant: string) =>
+  variantStyles[variant] || variantStyles.default
+
+const ReadMoreCard: React.FC<Props> = ({ text, href, className, variant }) => {
+  const style = getStyle(variant)
+
+  console.log(style.outer)
+
   return (
-    <a href={href} sx={outerElementSx} className={className}>
-      <div sx={{ flexGrow: 1 }}>{text}</div>
-      <div sx={{ flexShrink: 0, paddingLeft: 'md' }}>
+    <a href={href} sx={style.outer} className={className}>
+      <div sx={style.text}>{text}</div>
+      <div sx={style.icon}>
         <Icon glyph={'arrow'} color={'primary'} direction={'right'} size={18} />
       </div>
     </a>
