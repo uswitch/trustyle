4bd994eafb4ec130989eeabd4c78fe0d08c15bfd
meghaniankov <meghan.e.mccue@gmail.com>
2020-06-29 16:47:30 +0100

    refactor: move input-alert styling under elements in themes for all brands

--- src/elements/input-alert/src/index.tsx
+++ src/elements/input-alert/src/index.tsx
@@ -17,19 +17,19 @@ const ARROW_SIZE = 8
 export const InputAlert: React.FC<Props> = ({ type, children, ...props }) => {
   const { theme }: any = useThemeUI()
 
-  const padding = get(theme, 'input-alert.padding')
-  const notificationColor = get(theme, 'input-alert.notificationColor')
-  const alertColor = get(theme, 'input-alert.alertColor')
+  const padding = get(theme, 'elements.input-alert.padding')
+  const notificationColor = get(theme, 'elements.input-alert.notificationColor')
+  const alertColor = get(theme, 'elements.input-alert.alertColor')
 
   return (
     <div
       {...props}
       sx={{
-        variant: 'input-alert',
+        variant: 'elements.input-alert',
         marginTop: `calc(${ARROW_SIZE}px + ${padding}px)`,
         backgroundColor: type === 'alert' ? alertColor : notificationColor,
         '&:before': {
-          variant: 'input-alert.before',
+          variant: 'elements.input-alert.before',
           top: -ARROW_SIZE,
           borderLeft: `${ARROW_SIZE}px solid transparent`,
           borderRight: `${ARROW_SIZE}px solid transparent`,
