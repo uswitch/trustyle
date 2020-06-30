f83c3213df66ac0e90504904a938a535e2311097
meghaniankov <meghan.e.mccue@gmail.com>
2020-06-30 16:30:13 +0100

    feat: move styling to themes for all brands, add text prop, update money theme
    feat: edit money theme

--- src/elements/sponsored-by-tag/src/stories.tsx
+++ src/elements/sponsored-by-tag/src/stories.tsx
@@ -11,15 +11,39 @@ export default {
   title: 'Elements|Sponsored by tag'
 }
 
-export const ExampleWithKnobs = () => {
+export const ExampleWithDefaultText = () => {
   const providerLogo: string = text(
     'Provider logo url',
     'https://uswitch-mobiles-contentful.imgix.net/kf81nsuntxeb/5eyE4LyswwqIYk0mIsE820/dc0774e3e62d7b39ddeb1729d823a8da/Logo_-_three.png'
   )
+
   return <SponsoredByTag providerLogoSrc={providerLogo} />
 }
 
-ExampleWithKnobs.story = {
+export const ExampleWithTextProp = () => {
+  const providerLogo: string = text(
+    'Provider logo url',
+    'https://uswitch-mobiles-contentful.imgix.net/kf81nsuntxeb/5eyE4LyswwqIYk0mIsE820/dc0774e3e62d7b39ddeb1729d823a8da/Logo_-_three.png'
+  )
+
+  const providerText: string = text('Text to display', 'This is example text')
+  return (
+    <React.Fragment>
+      <SponsoredByTag
+        providerLogoSrc={providerLogo}
+        providerText={providerText}
+      />
+    </React.Fragment>
+  )
+}
+
+ExampleWithDefaultText.story = {
+  parameters: {
+    percy: { skip: true }
+  }
+}
+
+ExampleWithTextProp.story = {
   parameters: {
     percy: { skip: true }
   }
@@ -28,7 +52,8 @@ ExampleWithKnobs.story = {
 export const AutomatedTests = () => {
   return (
     <AllThemes>
-      <ExampleWithKnobs />
+      <ExampleWithDefaultText />
+      <ExampleWithTextProp />
     </AllThemes>
   )
 }
