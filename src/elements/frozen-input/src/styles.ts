45f3af6792cfa0c608c8295f1afaa1a7046b6aa8
Barney Fox <barneyfoxuk@gmail.com>
2020-07-10 14:03:36 +0100

    input styling money

--- src/elements/frozen-input/src/styles.ts
+++ src/elements/frozen-input/src/styles.ts
@@ -21,20 +21,3 @@ export const value: SerializedStyles = css({
   textOverflow: 'ellipsis',
   width: `calc(100% - ${editIconWidth})`
 })
-
-export const edit: SerializedStyles = css({
-  background: 'none',
-  border: 'none',
-  cursor: 'pointer',
-  height: pxToRem(28),
-  padding: pxToRem(0, spacers.teal),
-  width: editIconWidth,
-  ':focus': {
-    outline: `2px solid ${colors.azure}`
-  },
-  '&::-moz-focus-inner': { border: 0 }
-})
-
-export const hidden: SerializedStyles = css({
-  display: 'none'
-})
