7d0cce9f94381be1068d64d9cf24f006eead3a33
Ruxandra Anghel <ruxandra.anghel@rvu.co.uk>
2020-07-10 13:47:40 +0100

    docs: Update docs to include information about automatic publishing

--- dangerfile.ts
+++ dangerfile.ts
@@ -5,7 +5,7 @@ message('Changed Files in this PR: \n - ' + modifiedMD)
 
 // Commits should start with feat, fix, major or chore
 danger.git.commits.forEach(commit => {
-  if (!commit.message.match(/^(feat:)|(fix:)|(major:)|(chore:)/g)) {
+  if (!commit.message.match(/^(feat:)|(fix:)|(major:)|(chore:)|(docs:)/g)) {
     fail(
       `Commit message '${commit.message}' does match the correct format. Please follow [conventional commits](https://www.conventionalcommits.org)`
     )
