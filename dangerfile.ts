e3f1b5e33cd1ed70a9bc5fe09329ef9b91d30f84
Ruxandra Anghel <ruxandra.anghel@rvu.co.uk>
2020-07-10 14:24:12 +0100

    chore: Flag added/removed files

--- dangerfile.ts
+++ dangerfile.ts
@@ -3,6 +3,12 @@ import { danger, fail, message } from 'danger'
 const modifiedMD = danger.git.modified_files.join('- ')
 message('Changed Files in this PR: \n - ' + modifiedMD)
 
+const addedFiles = danger.git.created_files.join(', ')
+message('Added Files in this PR: \n - ' + addedFiles)
+
+const removedFiles = danger.git.deleted_files.join(', ')
+message('Removed files in this PR: \n - ' + removedFiles)
+
 // Commits should start with feat, fix, major or chore
 danger.git.commits.forEach(commit => {
   if (!commit.message.match(/^(feat:)|(fix:)|(major:)|(chore:)|(docs:)/g)) {
