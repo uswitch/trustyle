6a01f487bd5630cc0312279b6bcef99181057716
Antonio Kamber <antonio.kamber@q.agency>
2020-07-17 12:19:34 +0200

    Updated author to use generic title and subtitle props

--- src/elements/author/src/index.tsx
+++ src/elements/author/src/index.tsx
@@ -2,111 +2,97 @@
 
 import * as React from 'react'
 import { jsx, Styled } from 'theme-ui'
-import dayjs from '@uswitch/trustyle.date'
 
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
-  name: string
-  role: string
-  editorName?: string
-  editorRole?: string
+  title: string
+  subtitle?: string
   authorImage?: React.ReactElement
-  date: number
-  authorUrl: string
-  editorUrl?: string
+  titleUrl: string
+  subtitleUrl?: string
   className?: string
 }
 
 const Author: React.FC<Props> = ({
-  name,
-  role,
-  editorName,
-  editorRole,
+  title,
+  subtitle,
   authorImage,
-  date,
-  authorUrl,
-  editorUrl,
+  titleUrl,
+  subtitleUrl,
   className
-}) => {
-  const authorString = role
-    ? `Written by ${name}, ${role}`
-    : `Written by ${name}`
-  return (
-    <div
-      sx={{
-        display: 'flex',
-        alignItems: 'center',
-        flexDirection: 'row',
-        variant: 'author.main'
-      }}
-      className={className}
-    >
-      {authorImage && (
-        <Styled.a
-          href={authorUrl}
+}) => (
+  <div
+    sx={{
+      display: 'flex',
+      alignItems: 'center',
+      flexDirection: 'row',
+      variant: 'author.main'
+    }}
+    className={className}
+  >
+    {authorImage && (
+      <Styled.a
+        href={titleUrl}
+        sx={{
+          flexShrink: 0,
+          height: [48, 56],
+          width: [48, 56],
+          marginRight: 'sm',
+          variant: 'author.image-link'
+        }}
+      >
+        <authorImage.type
+          {...authorImage.props}
           sx={{
-            flexShrink: 0,
-            height: [48, 56],
-            width: [48, 56],
-            marginRight: 'sm',
-            variant: 'author.image-link'
+            maxHeight: '100%',
+            maxWidth: '100%',
+            borderRadius: '50%',
+            variant: 'author.image'
           }}
-        >
-          <authorImage.type
-            {...authorImage.props}
-            sx={{
-              maxHeight: '100%',
-              maxWidth: '100%',
-              borderRadius: '50%',
-              variant: 'author.image'
-            }}
-          />
-        </Styled.a>
-      )}
+        />
+      </Styled.a>
+    )}
 
-      <div>
-        <Styled.h6
+    <div>
+      <Styled.h6
+        sx={{
+          marginTop: 0,
+          marginBottom: 'xxs',
+          fontWeight: 'bold',
+          paddingBottom: 0,
+          variant: 'author.heading'
+        }}
+        as="h2"
+      >
+        <a
+          href={titleUrl}
           sx={{
-            marginTop: 0,
-            marginBottom: 'xxs',
-            fontWeight: 'bold',
-            paddingBottom: 0,
-            variant: 'author.heading'
-          }}
-          as="h2"
-        >
-          <a
-            href={authorUrl}
-            sx={{
-              color: 'inherit',
-              textDecoration: 'none',
-              variant: 'author.link'
-            }}
-          >
-            {authorString}
-          </a>
-        </Styled.h6>
-        <Styled.p
-          sx={{
-            fontSize: 'xxs',
-            marginY: 0,
-            variant: 'author.text'
+            color: 'inherit',
+            textDecoration: 'none',
+            variant: 'author.link'
           }}
         >
-          <Styled.a
-            href={editorUrl}
-            sx={{
-              textDecoration: 'none',
-              variant: 'author.link'
-            }}
-          >
-            {editorName &&
-              `Edited by ${editorName}${editorRole ? `, ${editorRole}` : ''}, `}
-          </Styled.a>
-          {date !== undefined && dayjs(date).format('LL')}
-        </Styled.p>
-      </div>
+          {title}
+        </a>
+      </Styled.h6>
+      <Styled.p
+        sx={{
+          fontSize: 'xxs',
+          marginY: 0,
+          variant: 'author.text'
+        }}
+      >
+        <Styled.a
+          href={subtitleUrl}
+          sx={{
+            textDecoration: 'none',
+            variant: 'author.link'
+          }}
+        >
+          {subtitle}
+        </Styled.a>
+      </Styled.p>
     </div>
-  )
-}
+  </div>
+)
 
 export default Author
