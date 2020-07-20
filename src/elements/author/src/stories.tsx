2be1ec87d28d6a1707f4d9a6234cb7ab1c8cb8f7
Antonio Kamber <antonio.kamber@q.agency>
2020-07-20 16:55:03 +0200

    Added condensed variant example to story, condensed variant design

--- src/elements/author/src/stories.tsx
+++ src/elements/author/src/stories.tsx
@@ -11,32 +11,55 @@ export default {
   title: 'Elements|Author'
 }
 
-export const Example = () => {
-  const title = text('Title', 'Salman')
-  const subtitle = text('Subtitle', 'Job title')
-  const titleUrl = text('Title url', 'https://www.jonathanfielding.com/')
-  const subtitleUrl = text('Subtitle url', 'https://www.jonathanfielding.com/')
-  const image = (
-    <img src="https://placekitten.com/200/200?image=9" alt="Salman" />
-  )
+const title = text('Title', 'Salman')
+const subtitle = text('Subtitle', 'Job title')
+const titleUrl = text('Title url', 'https://www.jonathanfielding.com/')
+const subtitleUrl = text('Subtitle url', 'https://www.jonathanfielding.com/')
+const image = <img src="https://placekitten.com/200/200?image=9" alt="Salman" />
 
-  return (
-    <div>
-      <h3>Without subtitle</h3>
-      <Author title={title} authorImage={image} titleUrl={titleUrl} />
-      <h3>With subtitle</h3>
-      <Author
-        title={title}
-        subtitle={subtitle}
-        authorImage={image}
-        titleUrl={titleUrl}
-        subtitleUrl={subtitleUrl}
-      />
-    </div>
-  )
-}
+export const Example = () => (
+  <div>
+    <h3>Without subtitle</h3>
+    <Author title={title} authorImage={image} titleUrl={titleUrl} />
+    <h3>With subtitle</h3>
+    <Author
+      title={title}
+      subtitle={subtitle}
+      authorImage={image}
+      titleUrl={titleUrl}
+      subtitleUrl={subtitleUrl}
+    />
+  </div>
+)
 
 Example.story = {
+  parameters: {
+    percy: { skip: true }
+  }
+}
+
+export const CondensedExample = () => (
+  <div>
+    <h3>Without subtitle</h3>
+    <Author
+      variant="condensed"
+      title={title}
+      authorImage={image}
+      titleUrl={titleUrl}
+    />
+    <h3>With subtitle</h3>
+    <Author
+      variant="condensed"
+      title={title}
+      subtitle={subtitle}
+      authorImage={image}
+      titleUrl={titleUrl}
+      subtitleUrl={subtitleUrl}
+    />
+  </div>
+)
+
+CondensedExample.story = {
   parameters: {
     percy: { skip: true }
   }
@@ -46,6 +69,7 @@ export const AutomatedTests = () => {
   return (
     <AllThemes>
       <Example />
+      <CondensedExample />
     </AllThemes>
   )
 }
