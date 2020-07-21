dfbfbad501d5b4f2ee143a3944d9db52d774ca57
Antonio Kamber <antonio.kamber@q.agency>
2020-07-21 13:51:23 +0200

    Percy tests fix: bankrate theme, title variable in stories

--- src/elements/author/src/stories.tsx
+++ src/elements/author/src/stories.tsx
@@ -11,26 +11,30 @@ export default {
   title: 'Elements|Author'
 }
 
-const title = text('Title', 'Salman')
-const subtitle = text('Subtitle', 'Job title')
-const titleUrl = text('Title url', 'https://www.jonathanfielding.com/')
-const subtitleUrl = text('Subtitle url', 'https://www.jonathanfielding.com/')
-const image = <img src="https://placekitten.com/200/200?image=9" alt="Salman" />
+export const Example = () => {
+  const title = text('Title', 'Salman')
+  const subtitle = text('Subtitle', 'Job title')
+  const titleUrl = text('Title url', 'https://www.jonathanfielding.com/')
+  const subtitleUrl = text('Subtitle url', 'https://www.jonathanfielding.com/')
+  const image = (
+    <img src="https://placekitten.com/200/200?image=9" alt="Salman" />
+  )
 
-export const Example = () => (
-  <div>
-    <h3>Without subtitle</h3>
-    <Author title={title} authorImage={image} titleUrl={titleUrl} />
-    <h3>With subtitle</h3>
-    <Author
-      title={title}
-      subtitle={subtitle}
-      authorImage={image}
-      titleUrl={titleUrl}
-      subtitleUrl={subtitleUrl}
-    />
-  </div>
-)
+  return (
+    <div>
+      <h3>Without subtitle</h3>
+      <Author title={title} authorImage={image} titleUrl={titleUrl} />
+      <h3>With subtitle</h3>
+      <Author
+        title={title}
+        subtitle={subtitle}
+        authorImage={image}
+        titleUrl={titleUrl}
+        subtitleUrl={subtitleUrl}
+      />
+    </div>
+  )
+}
 
 Example.story = {
   parameters: {
@@ -38,26 +42,36 @@ Example.story = {
   }
 }
 
-export const CondensedExample = () => (
-  <div>
-    <h3>Without subtitle</h3>
-    <Author
-      variant="condensed"
-      title={title}
-      authorImage={image}
-      titleUrl={titleUrl}
-    />
-    <h3>With subtitle</h3>
-    <Author
-      variant="condensed"
-      title={title}
-      subtitle={subtitle}
-      authorImage={image}
-      titleUrl={titleUrl}
-      subtitleUrl={subtitleUrl}
-    />
-  </div>
-)
+export const CondensedExample = () => {
+  const title = text('Title', 'Salman')
+  const subtitle = text('Subtitle', 'Job title')
+  const titleUrl = text('Title url', 'https://www.jonathanfielding.com/')
+  const subtitleUrl = text('Subtitle url', 'https://www.jonathanfielding.com/')
+  const image = (
+    <img src="https://placekitten.com/200/200?image=9" alt="Salman" />
+  )
+
+  return (
+    <div>
+      <h3>Without subtitle</h3>
+      <Author
+        variant="condensed"
+        title={title}
+        authorImage={image}
+        titleUrl={titleUrl}
+      />
+      <h3>With subtitle</h3>
+      <Author
+        variant="condensed"
+        title={title}
+        subtitle={subtitle}
+        authorImage={image}
+        titleUrl={titleUrl}
+        subtitleUrl={subtitleUrl}
+      />
+    </div>
+  )
+}
 
 CondensedExample.story = {
   parameters: {
