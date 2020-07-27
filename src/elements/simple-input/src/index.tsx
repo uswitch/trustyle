3a1b83d01f6340d3f3f863c33907f6534fad4bad
meghaniankov <meghan.e.mccue@gmail.com>
2020-07-27 11:16:39 +0100

    feat: move form styling to themes

--- src/elements/simple-input/src/index.tsx
+++ src/elements/simple-input/src/index.tsx
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 
 import * as React from 'react'
-import { jsx, useThemeUI } from 'theme-ui'
+import { jsx } from 'theme-ui'
 
 export interface Props {
   hasError?: boolean | undefined
@@ -15,28 +15,13 @@ export interface Props {
 export const SimpleInput = React.forwardRef(
   (props: Props, ref?: React.Ref<HTMLInputElement>) => {
     const { hasError, type, placeholder, name, onChange, defaultValue } = props
-    const { theme }: any = useThemeUI()
-
-    const error = theme.colors[theme.elements.input?.error?.color]
-    const normal = theme.colors[theme.elements.input?.default?.color]
-    const focus = theme.colors[theme.elements.input?.focus?.color]
 
     return (
       <input
         sx={{
-          variant: 'elements.input.base',
-          padding: '16px',
-          border: 'solid 1px',
-          borderRadius: theme.elements.input.wrapper.borderRadius,
-          borderColor: hasError ? error : normal,
-          boxShadow: hasError
-            ? `inset 0 0 0 1px ${error}`
-            : theme.elements.input.formInput.boxShadow,
-          backgroundColor: theme.elements.input.wrapper.backgroundColor,
-          ':focus': {
-            boxShadow: `inset 0 0 0 1px ${focus}`,
-            borderColor: focus
-          }
+          variant: hasError
+            ? 'elements.simple-input.variants.error'
+            : 'elements.simple-input.base'
         }}
         name={name}
         type={type}
