4d7bb3dc03bc5ef62be3d621036e0e9378d61501
Zara-RVU <zara.najiballah@rvu.co.uk>
2020-08-25 12:25:22 +0100

    feat: new component icons added

--- src/elements/icon/src/stories.tsx
+++ src/elements/icon/src/stories.tsx
@@ -8,12 +8,14 @@ import { Direction, Glyph, Icon } from './'
 
 const glyphChoices: Glyph[] = [
   'arrow',
+  'arrow-circle',
   'bill',
   'calendar',
   'caret',
   'check',
   'clock',
   'close',
+  'checkmark',
   'conversation',
   'cross',
   'edit',
@@ -24,6 +26,7 @@ const glyphChoices: Glyph[] = [
   'four',
   'gas-electricity',
   'home',
+  'information',
   'letter',
   'location',
   'minus',
@@ -45,7 +48,8 @@ const glyphChoices: Glyph[] = [
   'thumb-up',
   'edit-journey',
   'star-outlined',
-  'star-filled'
+  'star-filled',
+  'warning'
 ]
 
 const directionChoices: Direction[] = ['up', 'down', 'right', 'left']
