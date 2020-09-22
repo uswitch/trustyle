ef7b45bfc3b6d952670a998580eea306ae1acc45
Josip Turić <josip.turic@q.agency>
2020-09-22 20:06:20 +0200

    fix: npm run eslint:fix

--- src/elements/button/src/index.tsx
+++ src/elements/button/src/index.tsx
@@ -4,6 +4,7 @@ import * as React from 'react'
 import { jsx, useThemeUI } from 'theme-ui'
 import { darken } from '@theme-ui/color'
 import get from '@uswitch/trustyle-utils.get'
+
 import { Glyph, Icon } from '../../icon/src'
 
 export type Variant =
