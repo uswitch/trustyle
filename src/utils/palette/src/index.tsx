593b7140f8d036230a611aa8cfa4331de8f3e8f7
Phil Helm <github@phelm.co.uk>
2020-10-27 15:08:51 +0000

    palette specificity fix

--- src/utils/palette/src/index.tsx
+++ src/utils/palette/src/index.tsx
@@ -39,7 +39,7 @@ export const Palette = ({ children, className, px = {}, ...props }: any) => {
     <props.as
       {...props}
       className={`palette ${className}`}
-      sx={{ py: '0.05px', ...sx }}
+      sx={{ py: '0.05px', '&.palette': { ...sx } }}
     >
       {children}
     </props.as>
