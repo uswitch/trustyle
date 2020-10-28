d0af1275f16b4cc48ebe19c865ab2096f572f359
Hrvoje Canadjija <hrvoje.canadjija@q.agency>
2020-10-28 09:34:56 +0100

    feat: create coutdown component base

--- src/elements/countdown/src/index.tsx
+++ src/elements/countdown/src/index.tsx
@@ -2,31 +2,73 @@
 
 import * as React from 'react'
 import { jsx } from 'theme-ui'
-// import dayjs from '@uswitch/trustyle.date'
 
+interface TimeLeft {
+  days?: number | undefined
+  hours?: number | undefined
+  minutes?: number | undefined
+  seconds?: number | undefined
+}
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
   until: number
-  // calculateTimeLeft: Function
 }
 
-// const calculateTimeLeft = () => {
-//   const difference = +new Date(`10/01/${year}`) - +new Date();
+const style = (variant?: string) => `elements.countdown.${variant || 'base'}`
 
-//   let timeLeft = {};
+const calculateTimeLeft = (until: number): TimeLeft => {
+  const difference = +new Date(until) - +new Date()
 
-// };
+  return difference
+    ? {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60)
+      }
+    : {}
+}
 
 const Countdown: React.FC<Props> = ({ until }) => {
-  // const now = dayjs()
-  // const endDate = dayjs(until)
+  const [timeLeft, setTimeLeft] = React.useState<TimeLeft>(
+    calculateTimeLeft(until)
+  )
 
-  // const [timeLeft, setTimeLeft] = React.useState(calculateTimeLeft());
+  React.useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft(until))
+    }, 1000)
 
-  return <div>{until}</div>
+    return () => clearTimeout(timer)
+  })
+
+  return (
+    <div
+      sx={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        variant: style()
+      }}
+    >
+      {Object.keys(timeLeft).map((interval, i, intervals) => (
+        <React.Fragment key={i}>
+          <div
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              variant: style('interval')
+            }}
+          >
+            {timeLeft[interval].toString().padStart(2, '0')}
+
+            <small sx={{ variant: style('small') }}>{interval}</small>
+          </div>
+
+          {intervals.length - 1 > i && ':'}
+        </React.Fragment>
+      ))}
+    </div>
+  )
 }
-
-// Countdown.calculateTimeLeft = () => {
-
-// }
 
 export default Countdown
