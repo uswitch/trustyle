cbe5c078945e0e78d44e945e3b49574343c87aa7
Raluca Ciucu <raluca.ciucu@gmail.com>
2021-02-09 15:56:37 +0000

    add logic to personalise sidenav and additional links with textColor

--- src/compounds/side-nav/src/stories.tsx
+++ src/compounds/side-nav/src/stories.tsx
@@ -1,7 +1,9 @@
 /** @jsx jsx */
 import * as React from 'react'
 import { jsx } from '@emotion/core'
-import { object, text } from '@storybook/addon-knobs'
+import { color, object , text } from '@storybook/addon-knobs'
+import { PaletteProvider } from '@uswitch/trustyle-utils.palette'
+
 
 import AllThemes from '../../../utils/all-themes'
 import SideNav from '../src'
@@ -63,6 +65,30 @@ const defaultAdditionalLinks = [
   }
 ]
 
+export const ExampleWithTextColour = () => {
+  const internalLinksTitle = text('Internal links title', 'In this guide')
+  const internalLinks = object('Internal links', defaultInternalLinks)
+  const additionalLinks = object('Aditional links', defaultAdditionalLinks)
+  const textColor = color('Text colour', '#ff00ff')
+
+  return (
+    <PaletteProvider value={{ textColor: textColor }}>
+      <SideNav
+        internalLinks={{
+          title: internalLinksTitle,
+          links: internalLinks
+        }}
+        additionalLinks={additionalLinks}
+      />
+    </PaletteProvider>
+  )
+}
+
+ExampleWithTextColour.story = {
+  parameters: {
+    percy: { skip: true }
+  }
+}
 export const ExampleWithKnobs = () => {
   const internalLinksTitle = text('Internal links title', 'In this guide')
   const internalLinks = object('Internal links', defaultInternalLinks)
