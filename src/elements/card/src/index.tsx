8c7efdef26e591b7a02450360d1e86ddffa81ce7
cisel <ciselatkn@gmail.com>
2021-06-11 17:21:52 +0100

    fix: broadband-deals horizontal variant

--- src/elements/card/src/index.tsx
+++ src/elements/card/src/index.tsx
@@ -77,9 +77,7 @@ const Card: React.FC<Props> = ({
 
   const AspectRatioWrapper = (cond: boolean, children: React.ReactNode) =>
     cond ? (
-      <div sx={{ variant: 'compounds.card.aspectRatioWrapper' }}>
-        {children}
-      </div>
+      <div sx={{ variant: styles('aspectRatioWrapper') }}>{children}</div>
     ) : (
       children
     )
