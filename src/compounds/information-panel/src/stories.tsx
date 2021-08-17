451c10ef99e36c3d88306b504080056924749374
cisel <ciselatkn@gmail.com>
2021-08-17 23:29:19 +0300

    fix: test 3

--- src/compounds/information-panel/src/stories.tsx
+++ src/compounds/information-panel/src/stories.tsx
@@ -5,192 +5,192 @@ import { jsx } from 'theme-ui'
 import AllThemes from '../../../utils/all-themes'
 // import { CtaCell } from '../../legacy-product-table/src'
 
-// import {
-//   Eligibility,
-//   MoreInformationBlock,
-//   MoreInformationList,
-//   MoreInformationTable,
-//   MoreInformationText
-// } from './'
+import {
+  Eligibility,
+  MoreInformationBlock,
+  MoreInformationList,
+  MoreInformationTable
+  // MoreInformationText
+} from './'
 
-// export default {
-//   title: 'Compounds/InformationPanel'
-// }
+export default {
+  title: 'Compounds/InformationPanel'
+}
 
 // const clickableRow = 'https://www.money.co.uk'
-// const moreInfoLabel = 'Custom More Info Label'
+const moreInfoLabel = 'Custom More Info Label'
 // const moreInformationButtonClick = () => {
 //   console.log('More information clicked')
 // }
 
-// const list = (
-//   <MoreInformationList
-//     rows={[
-//       {
-//         value: 'You must meet the following criteria in order to get this loan:'
-//       },
-//       {
-//         label: 'UK Resident',
-//         value: true
-//       },
-//       {
-//         label: 'Guarantor Required',
-//         value: true
-//       },
-//       {
-//         label: 'Minimum age',
-//         value: 18
-//       },
-//       {
-//         label: 'Maximum age',
-//         value: 70
-//       },
-//       {
-//         label: 'Credit Rating Accepted',
-//         value: 'Good'
-//       }
-//     ]}
-//   />
-// )
+const list = (
+  <MoreInformationList
+    rows={[
+      {
+        value: 'You must meet the following criteria in order to get this loan:'
+      },
+      {
+        label: 'UK Resident',
+        value: true
+      },
+      {
+        label: 'Guarantor Required',
+        value: true
+      },
+      {
+        label: 'Minimum age',
+        value: 18
+      },
+      {
+        label: 'Maximum age',
+        value: 70
+      },
+      {
+        label: 'Credit Rating Accepted',
+        value: 'Good'
+      }
+    ]}
+  />
+)
 
-// const table = (
-//   <MoreInformationTable
-//     rows={[
-//       [
-//         {
-//           type: 'th',
-//           value: ''
-//         },
-//         {
-//           type: 'th',
-//           value: 'Gross rate'
-//         },
-//         {
-//           type: 'th',
-//           value: 'Gross rate'
-//         },
-//         {
-//           type: 'th',
-//           value: 'AER'
-//         },
-//         {
-//           type: 'th',
-//           value: 'AER'
-//         }
-//       ],
-//       [
-//         {
-//           type: 'td',
-//           value: ''
-//         },
-//         {
-//           type: 'td',
-//           value: 'Excluding bonus'
-//         },
-//         {
-//           type: 'td',
-//           value: 'Including bonus'
-//         },
-//         {
-//           type: 'td',
-//           value: 'Excluding bonus'
-//         },
-//         {
-//           type: 'td',
-//           value: 'Including bonus'
-//         }
-//       ],
-//       [
-//         {
-//           type: 'td',
-//           value: '£1'
-//         },
-//         {
-//           type: 'td',
-//           value: '0.20%'
-//         },
-//         {
-//           type: 'td',
-//           value: '0.20%'
-//         },
-//         {
-//           type: 'td',
-//           value: '0.20%'
-//         },
-//         {
-//           type: 'td',
-//           value: '0.20%'
-//         }
-//       ],
-//       [
-//         {
-//           type: 'td',
-//           value: '£1,000'
-//         },
-//         {
-//           type: 'td',
-//           value: '0.65%'
-//         },
-//         {
-//           type: 'td',
-//           value: '0.65%'
-//         },
-//         {
-//           type: 'td',
-//           value: '0.65%'
-//         },
-//         {
-//           type: 'td',
-//           value: '0.65%'
-//         }
-//       ],
-//       [
-//         {
-//           type: 'td',
-//           value: '£10,000'
-//         },
-//         {
-//           type: 'td',
-//           value: '0.95%'
-//         },
-//         {
-//           type: 'td',
-//           value: '0.95%'
-//         },
-//         {
-//           type: 'td',
-//           value: '0.95%'
-//         },
-//         {
-//           type: 'td',
-//           value: '0.95%'
-//         }
-//       ],
-//       [
-//         {
-//           type: 'td',
-//           value: '£50,000'
-//         },
-//         {
-//           type: 'td',
-//           value: '1.00%'
-//         },
-//         {
-//           type: 'td',
-//           value: '1.00%'
-//         },
-//         {
-//           type: 'td',
-//           value: '1.00%'
-//         },
-//         {
-//           type: 'td',
-//           value: '1.00%'
-//         }
-//       ]
-//     ]}
-//   />
-// )
+const table = (
+  <MoreInformationTable
+    rows={[
+      [
+        {
+          type: 'th',
+          value: ''
+        },
+        {
+          type: 'th',
+          value: 'Gross rate'
+        },
+        {
+          type: 'th',
+          value: 'Gross rate'
+        },
+        {
+          type: 'th',
+          value: 'AER'
+        },
+        {
+          type: 'th',
+          value: 'AER'
+        }
+      ],
+      [
+        {
+          type: 'td',
+          value: ''
+        },
+        {
+          type: 'td',
+          value: 'Excluding bonus'
+        },
+        {
+          type: 'td',
+          value: 'Including bonus'
+        },
+        {
+          type: 'td',
+          value: 'Excluding bonus'
+        },
+        {
+          type: 'td',
+          value: 'Including bonus'
+        }
+      ],
+      [
+        {
+          type: 'td',
+          value: '£1'
+        },
+        {
+          type: 'td',
+          value: '0.20%'
+        },
+        {
+          type: 'td',
+          value: '0.20%'
+        },
+        {
+          type: 'td',
+          value: '0.20%'
+        },
+        {
+          type: 'td',
+          value: '0.20%'
+        }
+      ],
+      [
+        {
+          type: 'td',
+          value: '£1,000'
+        },
+        {
+          type: 'td',
+          value: '0.65%'
+        },
+        {
+          type: 'td',
+          value: '0.65%'
+        },
+        {
+          type: 'td',
+          value: '0.65%'
+        },
+        {
+          type: 'td',
+          value: '0.65%'
+        }
+      ],
+      [
+        {
+          type: 'td',
+          value: '£10,000'
+        },
+        {
+          type: 'td',
+          value: '0.95%'
+        },
+        {
+          type: 'td',
+          value: '0.95%'
+        },
+        {
+          type: 'td',
+          value: '0.95%'
+        },
+        {
+          type: 'td',
+          value: '0.95%'
+        }
+      ],
+      [
+        {
+          type: 'td',
+          value: '£50,000'
+        },
+        {
+          type: 'td',
+          value: '1.00%'
+        },
+        {
+          type: 'td',
+          value: '1.00%'
+        },
+        {
+          type: 'td',
+          value: '1.00%'
+        },
+        {
+          type: 'td',
+          value: '1.00%'
+        }
+      ]
+    ]}
+  />
+)
 
 // const infoText = (
 //   <MoreInformationText
@@ -200,17 +200,17 @@ import AllThemes from '../../../utils/all-themes'
 //   />
 // )
 
-// const eligibilityContent = (
-//   <MoreInformationBlock title="Eligibility" key="key-1">
-//     {list}
-//   </MoreInformationBlock>
-// )
+const eligibilityContent = (
+  <MoreInformationBlock title="Eligibility" key="key-1">
+    {list}
+  </MoreInformationBlock>
+)
 
-// const ratesContent = (
-//   <MoreInformationBlock title="Rate tiers" key="key-2">
-//     {table}
-//   </MoreInformationBlock>
-// )
+const ratesContent = (
+  <MoreInformationBlock title="Rate tiers" key="key-2">
+    {table}
+  </MoreInformationBlock>
+)
 
 // const textContent = (
 //   <MoreInformationBlock title="Repayment" key="key-3">
@@ -219,39 +219,37 @@ import AllThemes from '../../../utils/all-themes'
 // )
 
 // const moreInformationButton = (
-//   <CtaCell
-//     styles={{
-//       width: '100%',
-//       fontSize: '14px',
-//       marginBottom: 0
-//     }}
-//     onClick={moreInformationButtonClick}
-//     href={clickableRow}
-//     variant="eligibility"
-//   >
-//     See Deal
-//   </CtaCell>
+//   // <CtaCell
+//   //   styles={{
+//   //     width: '100%',
+//   //     fontSize: '14px',
+//   //     marginBottom: 0
+//   //   }}
+//   //   onClick={moreInformationButtonClick}
+//   //   href={clickableRow}
+//   //   variant="eligibility"
+//   // >
+//   //   See Deal
+//   // </CtaCell>
 // )
 
-// const eligibity = (
-//   <Eligibility
-//     moreInformationPanel={[eligibilityContent, ratesContent]}
-//     moreInformationLabel={moreInfoLabel}
-//     moreInformationButton={moreInformationButton}
-//   />
-// )
+const eligibity = (
+  <Eligibility
+    moreInformationPanel={[eligibilityContent, ratesContent]}
+    moreInformationLabel={moreInfoLabel}
+  />
+)
 
 // const eligibityWithText = (
 //   <Eligibility
 //     moreInformationPanel={[eligibilityContent, textContent]}
 //     moreInformationLabel={moreInfoLabel}
-//     moreInformationButton={moreInformationButton}
 //   />
 // )
 
-// export const ExampleWithEligibility = () => (
-//   <React.Fragment>{eligibity}</React.Fragment>
-// )
+export const ExampleWithEligibility = () => (
+  <React.Fragment>{eligibity}</React.Fragment>
+)
 
 // export const ExampleWithText = () => (
 //   <React.Fragment>{eligibityWithText}</React.Fragment>
@@ -266,9 +264,8 @@ import AllThemes from '../../../utils/all-themes'
 export const AutomatedTests = () => {
   return (
     <AllThemes>
-      <p>Hello</p>
-      {/* <ExampleWithEligibility />
-      <ExampleWithText /> */}
+      <ExampleWithEligibility />
+      {/* <ExampleWithText /> */}
     </AllThemes>
   )
 }
