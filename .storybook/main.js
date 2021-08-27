044e0525930d2b44df95ba209d47969c5c881816
Marko Arthofer <markoarthofer95@gmail.com>
2021-08-27 09:28:47 +0200

    fix: reveted changes on some packages, fixed some small bugs on themes

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,21 +1,4 @@
 const path = require('path')
-const fs = require('fs')
-
-function getPackageDir(filepath) {
-  let currDir = path.dirname(require.resolve(filepath))
-  while (true) {
-    if (fs.existsSync(path.join(currDir, 'package.json'))) {
-      return currDir
-    }
-    const { dir, root } = path.parse(currDir)
-    if (dir === root) {
-      throw new Error(
-        `Could not find package.json in the parent directories starting from ${filepath}.`
-      )
-    }
-    currDir = dir
-  }
-}
 
 let stories = ['../src/**/*.stories.js', '../src/**/stories.tsx']
 
@@ -36,9 +19,6 @@ module.exports = {
   ],
   webpackFinal: async config => {
     config.resolve.alias = {
-      // '@emotion/core': getPackageDir('@emotion/react'),
-      // '@emotion/styled': getPackageDir('@emotion/styled'),
-      // 'emotion-theming': getPackageDir('@emotion/react')
       '@emotion/core': path.resolve(
         __dirname,
         '../node_modules/@emotion/react'
