7cd90598c574075a25d57f659dc8e40310464d55
cisel <ciselatkn@gmail.com>
2021-09-15 17:23:51 +0100

    feat: add additional compliance text to advert

--- src/compounds/advert/src/stories.tsx
+++ src/compounds/advert/src/stories.tsx
@@ -74,6 +74,41 @@ ExampleWithKnobs.story = {
   }
 }
 
+export const ExampleWithComplianceText = () => {
+  return (
+    <Hero>
+      <Row>
+        <Col span={[12, 5]}>
+          <Themed.h1 sx={{ marginTop: 0, fontSize: ['lg', 'xl', 'xxl'] }}>
+            Hero Advert
+          </Themed.h1>
+          <Themed.p sx={{ marginTop: 0 }}>
+            Lorem ipsum, or lipsum as it is sometimes known, is dummy text used
+            in laying out print, graphic or web designs.
+          </Themed.p>
+        </Col>
+        <Col span={[12, 6]}>
+          <Advert
+            headline="Compare business cards & loans"
+            body="Indicative offers from 45+ lenders in 3 mins. Never pay more than going direct. Will not affect your credit rating."
+            badgeText="Exclusive loans deals"
+            footer="Available to businesses trading for a minimum of 12 months with at least £80,000 turnover."
+            logo="https://cdn.money.co.uk/images/logos/129x75/funding-xchange.CDN6038ea49@2x.png"
+            link="https://money.co.uk?tracking=false"
+            complianceText="Capital at risk"
+          />
+        </Col>
+      </Row>
+    </Hero>
+  )
+}
+
+ExampleWithComplianceText.story = {
+  parameters: {
+    percy: { skip: true }
+  }
+}
+
 export const AutomatedTests = () => {
   return (
     <AllThemes>
@@ -83,6 +118,7 @@ export const AutomatedTests = () => {
         badgeText="Exclusive loans deals"
         footer="Available to businesses trading for a minimum of 12 months with at least £80,000 turnover."
         logo="https://cdn.money.co.uk/images/logos/129x75/funding-xchange.CDN6038ea49@2x.png"
+        complianceText="Capital at risk"
       />
     </AllThemes>
   )
