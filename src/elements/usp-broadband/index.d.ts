7a717927d8b3b49b7a1bce4cf41f27593be4aca3
Josh Benson <josh.benson@rvu.co.uk>
2021-09-29 16:33:53 +0100

    added index as type

--- src/elements/usp-broadband/index.d.ts
+++ src/elements/usp-broadband/index.d.ts
@@ -1,1 +1,19 @@
-declare module '@uswitch/spark.badge'
+declare module '@uswitch/spark.badge' {
+  interface BadgeProps {
+    variant?:
+      | 'base'
+      | 'error'
+      | 'warning'
+      | 'success'
+      | 'light'
+      | 'dark'
+      | 'primary'
+    size?: 'small' | 'medium' | 'large'
+    leftIcon?: JSX.Element
+    children?: React.ReactNode
+    css?: any
+    sx?: any
+  }
+
+  export const Badge: (props: BadgeProps) => JSX.Element
+}
