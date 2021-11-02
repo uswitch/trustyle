340e6e4f86a3b0eef6a1b488dc27e1daa99c3931
Marko Arthofer <markoarthofer95@gmail.com>
2021-11-02 14:05:12 +0100

    fix: removed billboard variant prop. added theme variant that defines background-position for both money and uswitch

--- src/compounds/billboard/src/stories.tsx
+++ src/compounds/billboard/src/stories.tsx
@@ -201,7 +201,6 @@ export const ExampleWithBgImage = () => {
       breadcrumbs={breadcrumbs}
       primaryContent={primaryContent}
       bgImage={bgImage}
-      verticalAlign={'bottom'}
     />
   )
 }
